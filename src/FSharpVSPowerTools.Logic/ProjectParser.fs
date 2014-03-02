25910f2e2adf5d22632ee33321203dde49fdcb61
vasily-kirichenko <vaskir@gmail.com>
2014-03-02 20:55:33 +0400

    reformat code according to our style

--- src/FSharpVSPowerTools.Logic/ProjectParser.fs
+++ src/FSharpVSPowerTools.Logic/ProjectParser.fs
@@ -12,10 +12,10 @@ open Microsoft.Build.BuildEngine
 module ProjectParser = 
     [<NoComparison>]
     type ProjectResolver = 
-        { project : Project
-          loadtime : DateTime }
+        { project: Project
+          loadtime: DateTime }
     
-    let load (uri : string) : Option<ProjectResolver> = 
+    let load (uri: string): Option<ProjectResolver> = 
         let p = Project()
         try 
             p.Load(uri)
@@ -25,11 +25,11 @@ module ProjectParser =
             Debug.WriteLine(sprintf "[Project System] %O exception occurs." e)
             None
     
-    let getFileName (p : ProjectResolver) : string = p.project.FullFileName
-    let getLoadTime (p : ProjectResolver) : DateTime = p.loadtime
-    let getDirectory (p : ProjectResolver) : string = IO.Path.GetDirectoryName p.project.FullFileName
+    let getFileName (p: ProjectResolver): string = p.project.FullFileName
+    let getLoadTime (p: ProjectResolver): DateTime = p.loadtime
+    let getDirectory (p: ProjectResolver): string = IO.Path.GetDirectoryName p.project.FullFileName
     
-    let getFiles (p : ProjectResolver) : string array = 
+    let getFiles (p: ProjectResolver): string array = 
         let fs = p.project.GetEvaluatedItemsByName("Compile")
         let dir = getDirectory p
         [| for f in fs do
@@ -43,19 +43,19 @@ module ProjectParser =
     // We can therefore build ResolveReferences, which depends on both of them,
     // or [|"ResolveProjectReferences";"ResolveAssemblyReferences"|]. These seem
     // to be equivalent. See Microsoft.Common.targets if you want more info.
-    let getReferences (p : ProjectResolver) : string array = 
+    let getReferences (p: ProjectResolver): string array = 
         ignore <| p.project.Build([| "ResolveReferences" |])
         [| for i in p.project.GetEvaluatedItemsByName("ResolvedFiles") do
                yield "-r:" + i.FinalItemSpec |]
     
-    let getOptionsWithoutReferences (p : ProjectResolver) : string array = 
+    let getOptionsWithoutReferences (p: ProjectResolver): string array = 
         let getprop s = p.project.GetEvaluatedProperty s
         
-        let split (s : string) (cs : char []) = 
+        let split (s: string) (cs: char []) = 
             if s = null then [||]
             else s.Split(cs, StringSplitOptions.RemoveEmptyEntries)
         
-        let getbool (s : string) = 
+        let getbool (s: string) = 
             match (Boolean.TryParse s) with
             | (true, result) -> result
             | (false, _) -> false
