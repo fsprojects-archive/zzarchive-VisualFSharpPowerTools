3840a45e7a0b8ffd4bdc528c4560ce571068aece
Denis Ok <denis.ok77@gmail.com>
2014-03-05 12:48:57 +0100

    Syntax coloring tagger

---
+++ src/FSharpVSPowerTools.Logic/SyntaxColorTagger.fs
@@ -0,0 +1,15 @@
+﻿namespace FSharpVSPowerTools.SyntaxColoring
+
+open System
+open System.Collections.Generic
+open Microsoft.VisualStudio.Text
+open Microsoft.VisualStudio.Text.Editor
+open Microsoft.VisualStudio.Text.Tagging
+open Microsoft.VisualStudio.Text.Operations
+open FSharpVSPowerTools
+open FSharpVSPowerTools.ProjectSystem
+
+
+// Reference at http://social.msdn.microsoft.com/Forums/vstudio/en-US/8e0f71f6-4794-4f0e-9a63-a8b55bc22e00/predefined-textmarkertag?forum=vsx
+type TypeColorTag() = 
+    inherit TextMarkerTag("MarkerFormatDefinition/HighlightedReference")
