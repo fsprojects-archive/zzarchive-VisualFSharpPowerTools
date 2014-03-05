6d4e18375f302b3b3cb6a3b90cbacf40ae96d517
Denis Ok <denis.ok77@gmail.com>
2014-03-05 13:10:00 +0100

    SyntaxColor tagger skeleton

--- src/FSharpVSPowerTools.Logic/SyntaxColorTagger.fs
+++ src/FSharpVSPowerTools.Logic/SyntaxColorTagger.fs
@@ -12,4 +12,15 @@ open FSharpVSPowerTools.ProjectSystem
 
 // Reference at http://social.msdn.microsoft.com/Forums/vstudio/en-US/8e0f71f6-4794-4f0e-9a63-a8b55bc22e00/predefined-textmarkertag?forum=vsx
 type TypeColorTag() = 
-    inherit TextMarkerTag("MarkerFormatDefinition/HighlightedReference")
+    inherit TextMarkerTag("SymbolDefinitionClassificationFormat")
+
+// Reference at http://msdn.microsoft.com/en-us/library/vstudio/dd885121.aspx
+type SyntaxColorTagger() =
+    let tagsChanged = Event<_, _>()
+
+    interface ITagger<TypeColorTag> with
+        member x.GetTags (spans: NormalizedSnapshotSpanCollection): seq<ITagSpan<HighlightUsageTag>> =
+            Seq.empty
+
+        [<CLIEvent>]
+        member x.TagsChanged = tagsChanged.Publish
