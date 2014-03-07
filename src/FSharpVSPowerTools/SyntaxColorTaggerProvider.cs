0357581fb892575860df21928ba2612bf25985e7
Denis Ok <denis.ok77@gmail.com>
2014-03-07 12:05:02 +0100

    Add SyntaxColorTaggerProvider (MEF extension)

---
+++ src/FSharpVSPowerTools/SyntaxColorTaggerProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Shell;
+using EnvDTE;
+using FSharpVSPowerTools.SyntaxColoring;
+
+namespace FSharpVSPowerTools
+{
+    [Export(typeof(IViewTaggerProvider))]
+    [ContentType("F#")]
+    [TagType(typeof(TypeColorTag))]
+    public class SyntaxColorTaggerProvider : IViewTaggerProvider
+    {
+        [Import]
+        internal ITextSearchService TextSearchService { get; set; }
+
+        public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag
+        {
+            //provide highlighting only on the top buffer 
+            if (textView.TextBuffer != buffer)
+                return null;
+
+            return new SyntaxColorTagger(textView, buffer, TextSearchService) as ITagger<T>;
+        }
+    }
+}
