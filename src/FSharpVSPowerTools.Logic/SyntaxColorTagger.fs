3fbc1a95d79709b9fca5230c57820d03aa5e6f10
Denis Ok <denis.ok77@gmail.com>
2014-03-07 11:58:25 +0100

    Syntax colorer POC attempt

--- src/FSharpVSPowerTools.Logic/SyntaxColorTagger.fs
+++ src/FSharpVSPowerTools.Logic/SyntaxColorTagger.fs
@@ -20,11 +20,32 @@ type SyntaxColorTagger(view: ITextView,
                        textSearchService: ITextSearchService) as self =
     let tagsChanged = Event<_, _>()
 
+    let lockObject = new Object()
+    let mutable wordSpans: NormalizedSnapshotSpanCollection = null
+
+    let synchronousUpdate (newSpans: NormalizedSnapshotSpanCollection) =
+        lock
+            lockObject
+            (fun () ->
+                wordSpans <- newSpans
+
+                tagsChanged.Trigger(self, 
+                                    new SnapshotSpanEventArgs(
+                                        new SnapshotSpan(
+                                            sourceBuffer.CurrentSnapshot,
+                                            0,
+                                            sourceBuffer.CurrentSnapshot.Length))))
+
     let caretPositionChangedHandler =
         let handler e f =
             let snapshot = sourceBuffer.CurrentSnapshot
-            let getText = snapshot.GetText()
-            ()
+//            let text = snapshot.GetText()
+            //Find the new spans
+            let mutable findData = new FindData("int", snapshot)
+            findData.FindOptions <- FindOptions.WholeWord ||| FindOptions.MatchCase
+
+            let wordSpans = new NormalizedSnapshotSpanCollection(textSearchService.FindAll(findData))
+            synchronousUpdate wordSpans
 
         EventHandler<_>(handler)
 
@@ -33,7 +54,22 @@ type SyntaxColorTagger(view: ITextView,
 
     interface ITagger<TypeColorTag> with
         member x.GetTags (spans: NormalizedSnapshotSpanCollection): seq<ITagSpan<TypeColorTag>> =
-            Seq.empty
+            seq {
+                // Hold on to a "snapshot" of the word spans and current word, so that we maintain the same
+                // collection throughout
+                if (spans.Count > 0 || wordSpans.Count > 0) then
+                    let wordSpansSnapshot =
+                        // If the requested snapshot isn't the same as the one our words are on, translate our spans to the expected snapshot 
+                        if spans.[0].Snapshot <> wordSpans.[0].Snapshot then
+                            new NormalizedSnapshotSpanCollection(
+                                wordSpans |> Seq.map (fun span -> span.TranslateTo(spans.[0].Snapshot, SpanTrackingMode.EdgeExclusive)))
+                        else
+                            wordSpans
+
+                    // Second, yield all the other words in the file 
+                    for (span: SnapshotSpan) in NormalizedSnapshotSpanCollection.Overlap(spans, wordSpansSnapshot) do
+                        yield (new TagSpan<_>(span, new TypeColorTag()) :> ITagSpan<_>)
+            }
 
         [<CLIEvent>]
         member x.TagsChanged = tagsChanged.Publish
