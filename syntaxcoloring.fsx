f9f0617852c64d312c57bbd529e8b2e3e0d4c31b
Denis Ok <denis.ok77@gmail.com>
2014-03-17 14:40:30 +0100

    Adding documentation skeleton

---
+++ syntaxcoloring.fsx
@@ -0,0 +1,19 @@
+﻿(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+
+(**
+Syntax coloring
+-----------------------
+
+We currently support:
+
+- type coloring
+- function coloring
+
+Colors of syntax constructs can be customized in "Tools --> Options --> Environment --> Fonts and Colors --> Display items" via:
+- F# User Types
+- F# functions
+
+*)
