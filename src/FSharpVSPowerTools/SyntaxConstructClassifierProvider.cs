e6f0837f049e1fdc4450f5fae9902f19f37c46d9
Denis Ok <denis.ok77@gmail.com>
2014-03-19 03:44:30 +0100

    How to get current theme...

--- src/FSharpVSPowerTools/SyntaxConstructClassifierProvider.cs
+++ src/FSharpVSPowerTools/SyntaxConstructClassifierProvider.cs
@@ -4,7 +4,9 @@ using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Utilities;
+using Microsoft.Win32;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Diagnostics;
 using System.Windows.Media;
@@ -43,9 +45,55 @@ namespace FSharpVSPowerTools
     // These colors are defined differently for debugging purpose
     // They are taken from https://github.com/leddt/visualstudio-colors-solarized/blob/master/vs2013/solarized-light.vssettings
     // In the final version, we should probably set a default theme with less number of colors.
+    public enum VsTheme
+    {
+        Unknown = 0,
+        Light,
+        Dark,
+        Blue
+    }
 
     static class ClassificationFormats
     {
+        private static readonly IDictionary<Guid, VsTheme> Themes = new Dictionary<Guid, VsTheme>()
+        {
+            {Guid.Parse("de3dbbcd-f642-433c-8353-8f1df4370aba"), VsTheme.Light},
+            {new Guid("1ded0138-47ce-435e-84ef-9ec1f439b749"), VsTheme.Dark},
+            {new Guid("a4d6a176-b948-4b29-8c66-53c97a1ed7d0"), VsTheme.Blue}
+        };
+
+        public static VsTheme GetCurrentTheme()
+        {
+            string themeId = GetThemeId();
+            Guid guidThemeId;
+            if (Guid.TryParse(themeId, out guidThemeId))
+            {
+                VsTheme theme;
+                if (Themes.TryGetValue(guidThemeId, out theme))
+                {
+                    return theme;
+                }
+            }
+
+            return VsTheme.Unknown;
+        }
+
+        public static string GetThemeId()
+        {
+            const string CategoryName = "General";
+            const string ThemePropertyName = "CurrentTheme";
+            string keyName = string.Format(@"Software\Microsoft\VisualStudio\12.0\{0}", CategoryName);
+
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyName))
+            {
+                if (key != null)
+                {
+                    return (string)key.GetValue(ThemePropertyName, string.Empty);
+                }
+            }
+
+            return null;
+        }
         [Export(typeof(EditorFormatDefinition))]
         [ClassificationType(ClassificationTypeNames = "FSharp.ReferenceType")]
         [Name("FSharp.ReferenceType")]
@@ -55,7 +103,11 @@ namespace FSharpVSPowerTools
             public FSharpReferenceTypeFormat()
             {
                 this.DisplayName = "F# User Types";
-                this.ForegroundColor = Color.FromRgb(43, 145, 175);
+
+                if (GetCurrentTheme() == VsTheme.Blue)
+                    this.ForegroundColor = Color.FromRgb(43, 145, 175);
+                else
+                    this.ForegroundColor = Colors.Yellow;
             }
         }
 
@@ -121,10 +173,11 @@ namespace FSharpVSPowerTools
                 return null;
             }
 
-            return buffer.Properties.GetOrCreateSingletonProperty(() => 
-                {
-                    return new SyntaxConstructClassifier(buffer, classificationRegistry, fsharpVsLanguageService, serviceProvider);
-                });
+            return buffer.Properties.GetOrCreateSingletonProperty(() =>
+            {
+                return new SyntaxConstructClassifier(buffer, classificationRegistry, fsharpVsLanguageService,
+                    serviceProvider);
+            });
         }
     }
 }
