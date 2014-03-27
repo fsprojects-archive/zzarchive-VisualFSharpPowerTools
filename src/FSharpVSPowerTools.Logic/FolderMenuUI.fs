f01db8534d7be99e11272ed71c660069c0c0e337
MarcinJuraszek <mail@marcinjuraszek.com>
2014-03-26 23:38:37 -0700

    MoveToFolder - commands, UI

---
+++ src/FSharpVSPowerTools.Logic/FolderMenuUI.fs
@@ -0,0 +1,58 @@
+﻿namespace FSharpVSPowerTools.Folders
+
+open System
+open System.Windows
+open System.ComponentModel
+open FSharpVSPowerTools
+
+[<RequireQualifiedAccess>]
+module FolderMenuUI =
+    let loadNewFolderDialog (viewModel: NewFolderNameDialogModel) =
+        let window = NewFolderNameDialog().CreateRoot()
+
+        // Provides access to "code behind" style work
+        let accessor = NewFolderNameDialog.Accessor(window)
+
+        // Use this until we are able to do validation directly
+        accessor.txtName.TextChanged.Add(fun _ ->
+            accessor.btnOk.IsEnabled <- not (viewModel :> INotifyDataErrorInfo).HasErrors
+             )
+        accessor.btnOk.Click.Add(fun _ -> 
+            match viewModel.Result with
+            | Choice1Of2 _ ->
+                window.DialogResult <- Nullable true
+                window.Close()
+            | Choice2Of2 errorMsg ->
+                window.DialogResult <- Nullable false
+                msgboxErr errorMsg)
+        accessor.btnCancel.Click.Add(fun _ -> 
+            window.DialogResult <- Nullable false
+            window.Close())
+        window.DataContext <- viewModel
+        window.Loaded.Add (fun _ -> accessor.txtName.SelectAll())
+        window
+        
+    let loadMoveToFolderDialog (viewModel: MoveToFolderDialogModel) =
+        let window = MoveToFolderDialog().CreateRoot()
+
+        // Provides access to "code behind" style work
+        let accessor = MoveToFolderDialog.Accessor(window)
+
+        // Use this until we are able to do validation directly
+        accessor.FoldersTree.SelectedItemChanged.Add(fun _ -> 
+            viewModel.Name <- (accessor.FoldersTree.SelectedItem :?> folder).Name
+            accessor.btnOk.IsEnabled <- viewModel.Result = Choice1Of2()
+        )
+        accessor.btnOk.Click.Add(fun _ -> 
+            match viewModel.Result with
+            | Choice1Of2 _ ->
+                window.DialogResult <- Nullable true
+                window.Close()
+            | Choice2Of2 errorMsg ->
+                window.DialogResult <- Nullable false
+                msgboxErr errorMsg)
+        accessor.btnCancel.Click.Add(fun _ -> 
+            window.DialogResult <- Nullable false
+            window.Close())
+        window.DataContext <- viewModel
+        window
---
+++ src/FSharpVSPowerTools.Logic/MoveToFolderDialog.fs
@@ -0,0 +1,44 @@
+﻿namespace FSharpVSPowerTools.Folders
+
+open System
+open System.IO
+open System.Windows
+open System.Windows.Input
+open System.ComponentModel
+open Microsoft.FSharp.Compiler.SourceCodeServices
+open FSharpVSPowerTools.ProjectSystem
+open FSharpVSPowerTools
+open FSharp.CompilerBinding
+
+type MoveToFolderDialog = FsXaml.XAML<"MoveToFolderDialog.xaml">
+
+[<NoEquality; NoComparison>]
+type folder = { Name: string; SubFolders: folder list }
+
+[<NoEquality; NoComparison>]
+type MoveToFolderDialogResources =
+    { NbOfItems: int
+      Root : folder list }
+
+type MoveToFolderDialogModel(resources :MoveToFolderDialogResources) =
+    let mutable name = ""
+
+    let validate (name :string) =
+        let name = name.Trim()
+        match name with
+        | "" -> Choice2Of2 Resource.validatingEmptyName
+        | _ -> Choice1Of2()
+
+    let mutable validationResult = validate name
+
+    member x.Result = validationResult
+
+    member x.WindowTitle = sprintf "Move %d item%s" resources.NbOfItems (match resources.NbOfItems with | 1 -> "" | _ -> "s")
+
+    member x.Root = resources.Root
+
+    member x.Name
+        with get() = name
+        and set (v :string) =
+            name <- v
+            validationResult <- validate name
