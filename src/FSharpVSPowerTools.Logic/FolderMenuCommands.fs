e17cef3e0fdaffaab64ce838c05223838ac3137d
MarcinJuraszek <mail@marcinjuraszek.com>
2014-03-26 23:38:37 -0700

    MoveToFolder - commands, UI

--- src/FSharpVSPowerTools.Logic/FolderMenuCommands.fs
+++ src/FSharpVSPowerTools.Logic/FolderMenuCommands.fs
@@ -19,24 +19,27 @@ open Reflection
 module PkgCmdConst = 
     let guidNewFolderCmdSet = new Guid("{e396b698-e00e-444b-9f5f-3dcb1ef74e63}")
     let cmdNewFolder = 0x1071
-    let cmdFolderRename = 0x1072
-    let cmdMoveFolderUp = 0x1073
-    let cmdMoveFolderDown = 0x1074
+    let guidMoveCmdSet = new Guid("{e396b698-e00e-444b-9f5f-3dcb1ef74e64}")
+    let cmdMoveFolderUp = 0x1070
+    let cmdMoveFolderDown = 0x1071
+    let cmdMoveToFolder = 0x1072
 
-type MoveAction =
+type VerticalMoveAction =
     | MoveUp
     | MoveDown
 
 type Action =
     | New
-    | MoveAction of MoveAction
+    | VerticalMoveAction of VerticalMoveAction
+    | MoveToFolder
 
 [<NoEquality; NoComparison>]
 type ActionInfo =
-    { Item : ProjectItem option
+    { Items : ProjectItem list
       Project : Project }
 
-type FSharpProjectSystemService(dte: DTE) = 
+[<Export>]
+type FSharpProjectSystemService [<ImportingConstructor>] (dte: DTE) = 
     let assemblyInfo =
         match VisualStudioVersion.fromDTEVersion dte.Version with
         | VisualStudioVersion.VS2012 ->
@@ -44,8 +47,7 @@ type FSharpProjectSystemService(dte: DTE) =
         | _ ->
             "FSharp.ProjectSystem.FSharp, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"    
     let asm = lazy try Assembly.Load(assemblyInfo)
-                   with _ ->
-                        AssemblyMissingException "FSharp.ProjectSystem.FSharp" |> logException |> raise
+                   with _ -> raise (AssemblyMissingException "FSharp.ProjectSystem.FSharp")
 
     let MSBuildUtilitiesType = lazy asm.Value.GetType("Microsoft.VisualStudio.FSharp.ProjectSystem.MSBuildUtilities")
 
@@ -57,8 +59,8 @@ type FSharpProjectSystemService(dte: DTE) =
 
 type FolderMenuCommands(dte:DTE2, mcs:OleMenuCommandService, shell:IVsUIShell) = 
 
-    let getSelectedItems() = VSUtils.getSelectedItemsFromSolutionExplorer dte |> List.ofSeq
-    let getSelectedProjects() = VSUtils.getSelectedProjectsFromSolutionExplorer dte |> List.ofSeq
+    let getSelectedItems() = VSUtils.getSelectedItemsFromSolutionExplorer dte |> Seq.toList
+    let getSelectedProjects() = VSUtils.getSelectedProjectsFromSolutionExplorer dte |> Seq.toList
 
     let rec getFolderNamesFromItems (items:ProjectItems) =
         seq { 
@@ -71,12 +73,33 @@ type FolderMenuCommands(dte:DTE2, mcs:OleMenuCommandService, shell:IVsUIShell) =
     let getfolderNamesFromProject (project: Project) =
         Set (getFolderNamesFromItems project.ProjectItems)
 
+    let rec getFoldersFromItems (items:ProjectItems) =
+        seq {
+            for item in items do
+                if VSUtils.isPhysicalFolder item then
+                    yield { Name = item.Name; SubFolders = getFoldersFromItems item.ProjectItems }
+        }
+        |> Seq.toList
+
+    let getFoldersFromProject (project: Project) =
+        [ { Name = project.Name; SubFolders = getFoldersFromItems project.ProjectItems } ]
+
     let getActionInfo() =
         let items = getSelectedItems()
         let projects = getSelectedProjects()
         match items, projects with
-        | [item], [] -> Some { ActionInfo.Item=Some(item); Project=item.ContainingProject }
-        | [], [project] -> Some { Item=None; Project=project }
+        | [], [project] -> Some { Items=[]; Project=project }
+        | [item], [] -> Some { Items=[item]; Project=item.ContainingProject }
+        | _::_, [] ->
+            let projects = 
+                items
+                |> List.toSeq
+                |> Seq.map (fun i -> i.ContainingProject)
+                |> Seq.distinctBy (fun p -> p.UniqueName)
+                |> Seq.toList
+            match projects with
+            | [project] -> Some { Items=items; Project=project }        // items from the same project
+            | _ -> None         // items from different projects
         | _, _ -> None
 
     let getNextItemImpl node parent =
@@ -125,18 +148,22 @@ type FolderMenuCommands(dte:DTE2, mcs:OleMenuCommandService, shell:IVsUIShell) =
     let getPreviousItem = getItem getPreviousItemImpl
     let getNextItem = getItem getNextItemImpl
 
-    let performMoveAction (info: ActionInfo) action =
-        let service = new FSharpProjectSystemService(dte :?> DTE)
-        let node = info.Item.Value?Node
-        let project = info.Project?Project
+    let performVerticalMoveAction (info: ActionInfo) action =
+        match info.Items with
+        | [item] ->
+            let service = new FSharpProjectSystemService(dte :?> DTE)
+            let node = item?Node
+            let project = info.Project?Project
 
-        match action with
-        | MoveAction.MoveUp -> service.MoveFolderUp node (getPreviousItem node) project
-        | MoveAction.MoveDown -> service.MoveFolderDown node (getNextItem node) project
+            match action with
+            | VerticalMoveAction.MoveUp -> service.MoveFolderUp node (getPreviousItem node) project
+            | VerticalMoveAction.MoveDown -> service.MoveFolderDown node (getNextItem node) project
 
-        project?SetProjectFileDirty(true)
-        project?ComputeSourcesAndFlags()
-        ()
+            project?SetProjectFileDirty(true)
+            project?ComputeSourcesAndFlags()
+            ()
+        | [] -> logErr "performVerticalMoveAction called with empty info.Items"
+        | _ -> logErr "performVerticalMoveAction called with more than one item in info.Items"
 
     let showDialog (wnd: Window) =
         try
@@ -147,20 +174,33 @@ type FolderMenuCommands(dte:DTE2, mcs:OleMenuCommandService, shell:IVsUIShell) =
         finally
             shell.EnableModeless(1) |> ignore
 
-    let askForNewFolderName resources = 
-        let model = NewFolderNameDialogModel(resources)
-        let wnd = UI.loadDialog model
+    let askForDestinationFolder resources =
+        let model = MoveToFolderDialogModel resources
+        let wnd = FolderMenuUI.loadMoveToFolderDialog model
         wnd.WindowStartupLocation <- WindowStartupLocation.CenterOwner
         let res = showDialog wnd
         match res with
         | Some true -> Some model.Name
         | _ -> None
 
+    let askForNewFolderName resources = 
+        let model = NewFolderNameDialogModel resources
+        let wnd = FolderMenuUI.loadNewFolderDialog model
+        wnd.WindowStartupLocation <- WindowStartupLocation.CenterOwner
+        let res = showDialog wnd
+        match res with
+        | Some true -> Some model.Name
+        | _ -> None
+
+    let performMoveToFolderAction (info: ActionInfo) name =
+        msgboxInf name
+        ()
+
     let performNewFolderAction (info: ActionInfo) name =
         let items = 
-            match info.Item with
-            | Some item -> item.ProjectItems
-            | None -> info.Project.ProjectItems
+            match info.Items with
+            | [item] -> item.ProjectItems
+            | _ -> info.Project.ProjectItems
         items.AddFolder name |> ignore
 
     let executeCommand (action:Action) = 
@@ -173,33 +213,47 @@ type FolderMenuCommands(dte:DTE2, mcs:OleMenuCommandService, shell:IVsUIShell) =
                     { WindowTitle = Resource.newFolderDialogTitle
                       FolderNames = getfolderNamesFromProject info.Project }
                 askForNewFolderName resources |> Option.iter (performNewFolderAction info)
-            | Action.MoveAction a -> performMoveAction info a
+            | Action.VerticalMoveAction a -> performVerticalMoveAction info a
+            | Action.MoveToFolder ->
+                let resources =
+                    { NbOfItems = List.length info.Items
+                      Root = getFoldersFromProject info.Project }
+                askForDestinationFolder resources |> Option.iter (performMoveToFolderAction info)
         | None -> fail "actionInfo is None"
 
-    let isMoveCommandEnabled item action =
-        match item with
-        | Some item ->
+    let isVerticalMoveCommandEnabled info action =
+        match info.Items with
+        | [item] ->
             if not (VSUtils.isPhysicalFolder item) then false
             else
                 let checkItem item =
                     item <> null && VSUtils.isPhysicalFileOrFolderKind (item?ItemTypeGuid?ToString("B"))
                 match action with
-                | MoveAction.MoveUp -> checkItem item?Node?PreviousSibling
-                | MoveAction.MoveDown -> checkItem item?Node?NextSibling
-        | None -> false
+                | VerticalMoveAction.MoveUp -> checkItem item?Node?PreviousSibling
+                | VerticalMoveAction.MoveDown -> checkItem item?Node?NextSibling
+        | _ -> false
 
-    let isNewFolderCommandEnabled item =
-        match item with 
-        | Some item -> VSUtils.isPhysicalFolder item
-        | None -> true
+    let isNewFolderCommandEnabled info =
+        match info.Items with 
+        | [item] -> VSUtils.isPhysicalFolder item
+        | [] -> true
+        | _ -> false
+
+    let isMoveToFolderCommandEnabled info =
+        match info.Items with
+        | [] -> false
+        | _ ->
+            let filesOnly = info.Items |> List.forall (fun i -> VSUtils.isPhysicalFile i)
+            filesOnly
 
     let isCommandEnabled (actionInfo: ActionInfo option) (action:Action) = 
         match actionInfo with
         | Some info ->
             if VSUtils.isFSharpProject info.Project then
                 match action with
-                | Action.New -> isNewFolderCommandEnabled info.Item
-                | Action.MoveAction a -> isMoveCommandEnabled info.Item a
+                | Action.New -> isNewFolderCommandEnabled info
+                | Action.VerticalMoveAction a -> isVerticalMoveCommandEnabled info a
+                | Action.MoveToFolder -> isMoveToFolderCommandEnabled info
             else
                 false
         | None -> false
@@ -211,9 +265,10 @@ type FolderMenuCommands(dte:DTE2, mcs:OleMenuCommandService, shell:IVsUIShell) =
         mcs.AddCommand(menuCommand)
     
     let setupNewFolderCommand = setupCommand PkgCmdConst.guidNewFolderCmdSet
+    let setupMoveCommand = setupCommand PkgCmdConst.guidMoveCmdSet
 
     member x.SetupCommands() = 
         setupNewFolderCommand PkgCmdConst.cmdNewFolder Action.New
-        setupNewFolderCommand PkgCmdConst.cmdMoveFolderUp (MoveAction MoveAction.MoveUp)
-        setupNewFolderCommand PkgCmdConst.cmdMoveFolderDown (MoveAction MoveAction.MoveDown)
-
+        setupMoveCommand PkgCmdConst.cmdMoveFolderUp (VerticalMoveAction VerticalMoveAction.MoveUp)
+        setupMoveCommand PkgCmdConst.cmdMoveFolderDown (VerticalMoveAction VerticalMoveAction.MoveDown)
+        setupMoveCommand PkgCmdConst.cmdMoveToFolder Action.MoveToFolder
