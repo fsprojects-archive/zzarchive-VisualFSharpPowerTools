abfb02265e7f40cdb418475333d48894bb3c465d
Phan Anh Dung <phananhdung309@yahoo.com>
2014-04-06 23:48:37 +0200

    Improve logging usage

--- src/FSharpVSPowerTools.Logic/RenameDialog.fs
+++ src/FSharpVSPowerTools.Logic/RenameDialog.fs
@@ -99,7 +99,7 @@ module UI =
                 window.Close()
             | Choice2Of2 errorMsg ->
                 window.DialogResult <- Nullable false
-                msgboxErr errorMsg)
+                msgboxError errorMsg)
         accessor.btnCancel.Click.Add(fun _ -> 
             window.DialogResult <- Nullable false
             window.Close())
