16f31592e4a574cddf2994a658eff172a1c679a4
Phan Anh Dung <phananhdung309@yahoo.com>
2014-04-25 23:13:40 +0200

    Update docs for release

--- src/FSharpVSPowerTools/GeneralOptionsControl.cs
+++ src/FSharpVSPowerTools/GeneralOptionsControl.cs
@@ -96,6 +96,9 @@ namespace FSharpVSPowerTools
             chbImplementInterface.Checked = OptionsPage.ImplementInterfaceEnabled;
             chbFolderOrganization.Checked = OptionsPage.FolderOrganizationEnabled;
             chbFindAllReferences.Checked = OptionsPage.FindAllReferencesEnabled;
+
+            // Temporarily hide this option
+            chbImplementInterface.Visible = false;
         }
 
         private void lblHome_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
