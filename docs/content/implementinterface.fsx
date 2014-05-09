48eb176681ab151a2370e5e7ae6b62ea97a725d5
dungpa <phananhdung309@yahoo.com>
2014-05-09 16:47:11 +0200

    Update docs for Implement Interface

---
+++ docs/content/implementinterface.fsx
@@ -0,0 +1,20 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+
+(**
+Implement interface
+-----------------------
+
+The feature is activated via a smart tagger on an interface identifier. 
+When the cursor is placed on an identifier of interface declaration or object expression, the smart tagger will hint users at generating relevant interfaces.
+
+Note that if the current line is `interface ISomeInterface with` or `new ISomeInterface with`, the code might not be parsable (i.e. no semantic highlighting for the current line).
+We recommend users to generate interfaces while the code is parsable e.g. after writing `interface ISomeInterface` or `new ISomeInterface`.
+
+See the demo screencast below.
+
+![Implement interface](img/implement_interface.gif)
+
+*)
