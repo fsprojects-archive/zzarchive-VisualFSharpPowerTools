edf8d25a0857dbad39ffff5d467ac875637a7826
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-06-26 12:27:50 +0200

    Tests for encapsulation of keywords - relates to #521

---
+++ tests/FSharpVSPowerTools.Core.Tests/RenameTests.fs
@@ -0,0 +1,23 @@
+﻿module FSharpVSPowerTools.Core.Tests.RenameTests
+
+open FSharpVSPowerTools
+open NUnit.Framework
+
+let encapsulate oldName = 
+    if List.exists ((=) oldName) Microsoft.FSharp.Compiler.Lexhelp.Keywords.keywordNames then "``" + oldName + "``"
+    else oldName
+
+let staysAsIs name = encapsulate name |> assertEqual name
+let wasEncapsulated name = encapsulate name |> assertEqual ("``" + name + "``")
+
+[<Test>]
+let ``should not encapsulate normal identifiers``() = 
+    staysAsIs "abc"
+    staysAsIs "abc1234"
+    staysAsIs "a_4"
+
+[<Test>]
+let ``should encapsulate keywords``() = 
+    wasEncapsulated "namespace"
+    wasEncapsulated "module"
+    wasEncapsulated "let"
