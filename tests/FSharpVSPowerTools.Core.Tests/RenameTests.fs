5f2729e4ff9dcb63c13b14d55eb1abccd87565dc
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-06-26 12:46:12 +0200

    Do not encapsulate already encapsulated identifiers - relates to #521

--- tests/FSharpVSPowerTools.Core.Tests/RenameTests.fs
+++ tests/FSharpVSPowerTools.Core.Tests/RenameTests.fs
@@ -3,11 +3,16 @@
 open FSharpVSPowerTools
 open NUnit.Framework
 
-let encapsulate oldName = 
-    let isKeyWord = List.exists ((=) oldName) Microsoft.FSharp.Compiler.Lexhelp.Keywords.keywordNames
+/// Encapsulates identifiers for rename operations if needed
+let encapsulate name = 
+    let delimiter = "``"
+    let specialChars = [" "; "!"]
+    let isKeyWord = List.exists ((=) name) Microsoft.FSharp.Compiler.Lexhelp.Keywords.keywordNames
+    let containsSpecialChar = List.exists (fun char -> name.Contains char) specialChars
 
-    if isKeyWord || oldName.Contains " " then "``" + oldName + "``"
-    else oldName
+    if name.StartsWith delimiter && name.EndsWith delimiter then name // already encapsulated
+    elif isKeyWord || containsSpecialChar then delimiter + name + delimiter
+    else name
 
 let staysAsIs name = encapsulate name |> assertEqual name
 let wasEncapsulated name = encapsulate name |> assertEqual ("``" + name + "``")
@@ -25,6 +30,11 @@ let ``should encapsulate keywords``() =
     wasEncapsulated "let"
 
 [<Test>]
-let ``should encapsulate blanks``() = 
+let ``should encapsulate special chars``() = 
     wasEncapsulated "this is a valid identifierer"
-    wasEncapsulated "look mum no hands!"
+    wasEncapsulated "look!" // reserved for future F#
+
+[<Test>]
+let ``should not encapsulate already encapsulated identifiers``() = 
+    staysAsIs "``this is already encapsulated``"
+    staysAsIs "``this``"
