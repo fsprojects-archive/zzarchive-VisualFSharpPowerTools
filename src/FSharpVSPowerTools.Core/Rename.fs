83c5e001a711ad2ee410e087821c6fbd629112b7
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-07-08 11:19:30 +0200

    Use existing isIdentifier logic

--- src/FSharpVSPowerTools.Core/Rename.fs
+++ src/FSharpVSPowerTools.Core/Rename.fs
@@ -1,12 +1,26 @@
 ﻿module FSharpVSPowerTools.Rename.Checks
 
+open Microsoft.FSharp.Compiler.PrettyNaming
+
+let private isDoubleBacktickIdent (s: string) =
+    if s.StartsWith("``") && s.EndsWith("``") && s.Length > 4 then
+        let inner = s.Substring("``".Length, s.Length - "````".Length)
+        not (inner.Contains("``"))
+    else false
+
+let isIdentifier (s: string) =
+    if isDoubleBacktickIdent s then
+        true
+    else
+        s |> Seq.mapi (fun i c -> i, c)
+          |> Seq.forall (fun (i, c) -> 
+                if i = 0 then IsIdentifierFirstCharacter c else IsIdentifierPartCharacter c) 
+
 /// Encapsulates identifiers for rename operations if needed
 let encapsulateIdentifier name = 
-    let delimiter = "``"
-    let specialChars = [" "; "!"]
-    let isKeyWord = List.exists ((=) name) Microsoft.FSharp.Compiler.Lexhelp.Keywords.keywordNames
-    let containsSpecialChar = List.exists (fun char -> name.Contains char) specialChars
+    let delimiter = "``"    
+    let isKeyWord = List.exists ((=) name) Microsoft.FSharp.Compiler.Lexhelp.Keywords.keywordNames    
 
     if name.StartsWith delimiter && name.EndsWith delimiter then name // already encapsulated
-    elif isKeyWord || containsSpecialChar then delimiter + name + delimiter
+    elif isKeyWord || not (isIdentifier name) then delimiter + name + delimiter
     else name
