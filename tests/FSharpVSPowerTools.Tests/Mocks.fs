4d56dc846d2e00afbc30a8d1bcaebfe928f85a2d
dungpa <phananhdung309@yahoo.com>
2014-08-03 11:01:52 +0200

    Fix VS unit tests

--- tests/FSharpVSPowerTools.Tests/Mocks.fs
+++ tests/FSharpVSPowerTools.Tests/Mocks.fs
@@ -47,3 +47,56 @@ let createSVsResourceManager() =
             member __.LoadResourceString2(pszAssemblyPath: string, culture: int, pszResourceName: string, pbstrValue: byref<string>): int = 
                 notimpl
     }
+
+let createVsSolutionBuildManager2() =
+    {
+        new IVsSolutionBuildManager2 with
+            member __.AdviseUpdateSolutionEvents(pIVsUpdateSolutionEvents: IVsUpdateSolutionEvents, pdwCookie: byref<uint32>): int = 
+                VSConstants.S_OK
+
+            member __.CalculateProjectDependencies(): int = notimpl
+            member __.CanCancelUpdateSolutionConfiguration(pfCanCancel: byref<int>): int = notimpl
+            member __.CancelUpdateSolutionConfiguration(): int = notimpl
+            member __.DebugLaunch(grfLaunch: uint32): int = notimpl
+            member __.FindActiveProjectCfg(pvReserved1: nativeint, pvReserved2: nativeint, pIVsHierarchy_RequestedProject: IVsHierarchy, ppIVsProjectCfg_Active: IVsProjectCfg []): int = notimpl
+            member __.GetProjectDependencies(pHier: IVsHierarchy, celt: uint32, rgpHier: IVsHierarchy [], pcActual: uint32 []): int = notimpl
+            member __.QueryBuildManagerBusy(pfBuildManagerBusy: byref<int>): int = notimpl
+            member __.QueryDebugLaunch(grfLaunch: uint32, pfCanLaunch: byref<int>): int = notimpl
+            member __.QueryProjectDependency(pHier: IVsHierarchy, pHierDependentOn: IVsHierarchy, pfIsDependentOn: byref<int>): int = notimpl
+            member __.SaveDocumentsBeforeBuild(pHier: IVsHierarchy, itemid: uint32, docCookie: uint32): int = notimpl
+            member __.StartSimpleUpdateProjectConfiguration(pIVsHierarchyToBuild: IVsHierarchy, pIVsHierarchyDependent: IVsHierarchy, pszDependentConfigurationCanonicalName: string, dwFlags: uint32, dwDefQueryResults: uint32, fSuppressUI: int): int = notimpl
+            member __.StartSimpleUpdateSolutionConfiguration(dwFlags: uint32, dwDefQueryResults: uint32, fSuppressUI: int): int = notimpl
+            member __.StartUpdateProjectConfigurations(cProjs: uint32, rgpHierProjs: IVsHierarchy [], dwFlags: uint32, fSuppressUI: int): int = notimpl
+            member __.StartUpdateSpecificProjectConfigurations(cProjs: uint32, rgpHier: IVsHierarchy [], rgpcfg: IVsCfg [], rgdwCleanFlags: uint32 [], rgdwBuildFlags: uint32 [], rgdwDeployFlags: uint32 [], dwFlags: uint32, fSuppressUI: int): int = notimpl
+                       
+            member __.UnadviseUpdateSolutionEvents(dwCookie: uint32): int = 
+                VSConstants.S_OK
+        
+            member __.UpdateSolutionConfigurationIsActive(pfIsActive: byref<int>): int = notimpl
+            member __.get_CodePage(puiCodePage: byref<uint32>): int = notimpl
+            member __.get_IsDebug(pfIsDebug: byref<int>): int = notimpl
+            member __.get_StartupProject(ppHierarchy: byref<IVsHierarchy>): int = notimpl
+            member __.put_CodePage(uiCodePage: uint32): int = notimpl
+            member __.put_IsDebug(fIsDebug: int): int = notimpl
+            member __.set_StartupProject(pHierarchy: IVsHierarchy): int = notimpl
+
+        interface IVsSolutionBuildManager with
+            member __.UnadviseUpdateSolutionEvents(dwCookie: uint32): int = notimpl     
+            member __.get_StartupProject(ppHierarchy: byref<IVsHierarchy>): int = notimpl 
+            member __.QueryDebugLaunch(grfLaunch: uint32, pfCanLaunch: byref<int>): int = notimpl
+            member __.put_CodePage(uiCodePage: uint32): int = notimpl
+            member __.put_IsDebug(fIsDebug: int): int = notimpl
+            member __.set_StartupProject(pHierarchy: IVsHierarchy): int = notimpl
+            member __.get_CodePage(puiCodePage: byref<uint32>): int = notimpl
+            member __.get_IsDebug(pfIsDebug: byref<int>): int = notimpl
+            member __.StartSimpleUpdateProjectConfiguration(pIVsHierarchyToBuild: IVsHierarchy, pIVsHierarchyDependent: IVsHierarchy, pszDependentConfigurationCanonicalName: string, dwFlags: uint32, dwDefQueryResults: uint32, fSuppressUI: int): int = notimpl
+            member __.CanCancelUpdateSolutionConfiguration(pfCanCancel: byref<int>): int = notimpl
+            member __.AdviseUpdateSolutionEvents(pIVsUpdateSolutionEvents: IVsUpdateSolutionEvents, pdwCookie: byref<uint32>): int = notimpl
+            member __.CancelUpdateSolutionConfiguration(): int = notimpl
+            member __.DebugLaunch(grfLaunch: uint32): int = notimpl
+            member __.FindActiveProjectCfg(pvReserved1: nativeint, pvReserved2: nativeint, pIVsHierarchy_RequestedProject: IVsHierarchy, ppIVsProjectCfg_Active: IVsProjectCfg []): int = notimpl
+            member __.GetProjectDependencies(pHier: IVsHierarchy, celt: uint32, rgpHier: IVsHierarchy [], pcActual: uint32 []): int = notimpl
+            member __.QueryBuildManagerBusy(pfBuildManagerBusy: byref<int>): int = notimpl
+            member __.UpdateSolutionConfigurationIsActive(pfIsActive: byref<int>): int = notimpl
+            member __.StartSimpleUpdateSolutionConfiguration(dwFlags: uint32, dwDefQueryResults: uint32, fSuppressUI: int): int = notimpl                                   
+    }
