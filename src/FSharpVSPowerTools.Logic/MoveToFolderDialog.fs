809184c2f87cb09e7d7b3736fd2c24c057c51c8c
Chris Dobson <dobby@chrisjdobson.co.uk>
2014-09-25 14:38:19 +0100

    Use new validation logic in folder dialogs

--- src/FSharpVSPowerTools.Logic/MoveToFolderDialog.fs
+++ src/FSharpVSPowerTools.Logic/MoveToFolderDialog.fs
@@ -2,6 +2,8 @@
 
 open System.IO
 open FSharpVSPowerTools
+open FSharp.ViewModule
+open FSharp.ViewModule.Validation
 
 type MoveToFolderDialog = FsXaml.XAML< "MoveToFolderDialog.xaml" >
 
@@ -15,36 +17,34 @@ type MoveToFolderDialogResources =
     { FileNames: string list
       Root: Folder list }
 
-type MoveToFolderDialogModel(resources: MoveToFolderDialogResources) = 
-    let mutable folder = None
-    
-    let validate (folder: Folder option) = 
-        match folder with
-        | None -> Choice2Of2 Resource.validatingEmptyName
-        | Some folder ->
-            match folder.Name with
-            | "" -> Choice2Of2 Resource.validatingEmptyName
-            | _ -> 
-                resources.FileNames 
-                |> List.map (fun fn -> Path.Combine (folder.FullPath, fn))
-                |> List.filter File.Exists
-                |> function
-                   | [] -> Choice1Of2()
-                   | existentFiles -> 
-                        Choice2Of2 (existentFiles 
-                                    |> List.reduce (fun acc fn -> acc + (sprintf "File %s already exists.\n" fn)))
-    
-    let mutable validationResult = validate folder
-    member x.Result = validationResult
-    
+type MoveToFolderDialogModel(resources: MoveToFolderDialogResources) as self = 
+    inherit ViewModelBase()
+
+    let folderSelectedAndExists newFolder =
+        match newFolder with
+            | None -> Some Resource.validatingEmptyName
+            | Some f ->
+                match f.Name with
+                    | null -> Some Resource.validatingEmptyName
+                    | "" -> Some Resource.validatingEmptyName
+                    | _ -> resources.FileNames
+                            |> List.map(fun fn -> Path.Combine(f.FullPath, fn))
+                            |> List.filter File.Exists
+                            |> function
+                               | [] -> None
+                               | files -> Some (files |> List.reduce (fun acc fn -> acc + (sprintf "File %s already exists.\n" fn)))
+
+    let validateFolder = 
+        validate "SelectedFolder" 
+            >> custom folderSelectedAndExists
+            >> result
+    let originalFolder : Folder option = None
+    let folder = self.Factory.Backing(<@@ self.SelectedFolder @@>, originalFolder, validateFolder)
+        
     member x.WindowTitle = 
         let fileCount = List.length resources.FileNames
         sprintf "%s - Move %d item%s" Resource.vsPackageTitle fileCount (if fileCount = 1 then "" else "s")
     
     member x.Root = resources.Root
     
-    member x.SelectedFolder 
-        with get () = folder
-        and set (v: Folder option) = 
-            folder <- v
-            validationResult <- validate folder
+    member x.SelectedFolder with get() = folder.Value and set (v) = folder.Value <- v
