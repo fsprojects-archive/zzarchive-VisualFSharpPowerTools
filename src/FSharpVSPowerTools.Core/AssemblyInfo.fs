c6822275f5392b5382c45cd1a6041c8172fea78e
dungpa <phananhdung309@yahoo.com>
2014-10-02 11:09:39 +0200

    Bump version to 1.5.0

--- src/FSharpVSPowerTools.Core/AssemblyInfo.fs
+++ src/FSharpVSPowerTools.Core/AssemblyInfo.fs
@@ -2,14 +2,13 @@
 open System.Reflection
 open System.Runtime.CompilerServices
 
+[<assembly: InternalsVisibleToAttribute("FSharpVSPowerTools.Core.Tests")>]
 [<assembly: AssemblyTitleAttribute("FSharpVSPowerTools.Core")>]
 [<assembly: AssemblyProductAttribute("FSharpVSPowerTools")>]
 [<assembly: AssemblyDescriptionAttribute("A collection of additional commands for F# in Visual Studio")>]
 [<assembly: AssemblyVersionAttribute("1.5.0")>]
 [<assembly: AssemblyFileVersionAttribute("1.5.0")>]
-[<assembly: InternalsVisibleToAttribute("FSharpVSPowerTools.Core.Tests")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] Version = "1.5.0"
-    
