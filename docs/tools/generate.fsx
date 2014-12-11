37c6496361d911a29ba6064ca1fd853864377aeb
Steffen Forkmann <sforkmann@gmail.com>
2014-12-11 12:23:56 +0100

    Docs generator should use no version in path

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -25,7 +25,7 @@ let info =
 #I "../../packages/FSharp.Formatting/lib/net40"
 #I "../../packages/RazorEngine/lib/net40"
 // Use this until FSharp.Formatting upgrades to a new version of FCS
-#I "../../packages/FSharp.Compiler.Service.0.0.61/lib/net45"
+#I "../../packages/FSharp.Compiler.Service/lib/net45"
 #r "../../packages/Microsoft.AspNet.Razor/lib/net40/System.Web.Razor.dll"
 #r "../../packages/FAKE/tools/FakeLib.dll"
 #r "RazorEngine.dll"
