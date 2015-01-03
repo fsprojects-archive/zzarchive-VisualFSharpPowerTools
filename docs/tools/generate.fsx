3681d521fc9f89ee27f754500bf2af64d3e73811
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-01-03 16:17:14 +0100

    New FSharp.Formatting uses latest FCS

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -24,8 +24,7 @@ let info =
 
 #I "../../packages/FSharp.Formatting/lib/net40"
 #I "../../packages/RazorEngine/lib/net40"
-// Use this local copy of FCS until FSharp.Formatting upgrades to a new version of FCS
-#I "../../lib"
+#I "../../packages/FSharp.Compiler.Service/lib/net40"
 #r "../../packages/Microsoft.AspNet.Razor/lib/net40/System.Web.Razor.dll"
 #r "../../packages/FAKE/tools/FakeLib.dll"
 #r "RazorEngine.dll"
