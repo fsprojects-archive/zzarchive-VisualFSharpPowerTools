a180977a4240182c473f6e4a4339e8820cfa2d48
dungpa <phananhdung309@yahoo.com>
2015-03-02 09:51:01 +0100

    Bump version again

--- src/FSharpVSPowerTools.Core/AssemblyInfo.fs
+++ src/FSharpVSPowerTools.Core/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("FSharpVSPowerTools.Core")>]
 [<assembly: AssemblyProductAttribute("FSharpVSPowerTools")>]
 [<assembly: AssemblyDescriptionAttribute("A collection of additional commands for F# in Visual Studio")>]
-[<assembly: AssemblyVersionAttribute("1.7.3")>]
-[<assembly: AssemblyFileVersionAttribute("1.7.3")>]
+[<assembly: AssemblyVersionAttribute("1.8.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.8.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.7.3"
+    let [<Literal>] Version = "1.8.0"
--- src/FSharpVSPowerTools/Properties/AssemblyInfo.cs
+++ src/FSharpVSPowerTools/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitleAttribute("FSharpVSPowerTools")]
 [assembly: AssemblyProductAttribute("FSharpVSPowerTools")]
 [assembly: AssemblyDescriptionAttribute("A collection of additional commands for F# in Visual Studio")]
-[assembly: AssemblyVersionAttribute("1.7.3")]
-[assembly: AssemblyFileVersionAttribute("1.7.3")]
+[assembly: AssemblyVersionAttribute("1.8.0")]
+[assembly: AssemblyFileVersionAttribute("1.8.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.7.3";
+        internal const string Version = "1.8.0";
     }
 }
