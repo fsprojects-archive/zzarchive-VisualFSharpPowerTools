17e6ea0062629140be8b438a47d23012cb1c40d2
dungpa <phananhdung309@yahoo.com>
2015-03-02 16:35:14 +0100

    Dispose resources in Go to Metadata feature
    Related to https://github.com/Microsoft/visualfsharp/pull/138

--- src/FSharpVSPowerTools.Logic/GoToDefinitionFilter.fs
+++ src/FSharpVSPowerTools.Logic/GoToDefinitionFilter.fs
@@ -17,6 +17,8 @@ open FSharpVSPowerTools.ProjectSystem
 open FSharpVSPowerTools.CodeGeneration
 open Microsoft.VisualStudio.Text
 open Microsoft.FSharp.Compiler.Range
+open EnvDTE
+open EnvDTE80
 
 type GoToDefinitionFilter(textDocument: ITextDocument,
                           view: IWpfTextView, 
@@ -40,7 +42,7 @@ type GoToDefinitionFilter(textDocument: ITextDocument,
                 match symbolUse with
                 | Some (fsSymbolUse, fileScopedCheckResults) ->
                     let lineStr = span.Start.GetContainingLine().GetText()
-                    let! findDeclResult = fileScopedCheckResults.GetDeclarationLocation(symbol.Line, symbol.RightColumn, lineStr, symbol.Text, false)
+                    let! findDeclResult = fileScopedCheckResults.GetDeclarationLocation(symbol.Line, symbol.RightColumn, lineStr, symbol.Text, preferSignature=false)
                     return Some (project, fileScopedCheckResults.GetUntypedAst(), span, fsSymbolUse, findDeclResult) 
                 | _ -> return None
             | _ -> return None
@@ -296,6 +298,9 @@ type GoToDefinitionFilter(textDocument: ITextDocument,
             }
         Async.StartInThreadPoolSafe (worker, cancelToken.Token)
 
+    static member ClearXmlDocCache() =
+        xmlDocCache.Clear()
+
     member val IsAdded = false with get, set
     member val NextTarget: IOleCommandTarget = null with get, set
 
@@ -316,4 +321,12 @@ type GoToDefinitionFilter(textDocument: ITextDocument,
                 prgCmds.[0].cmdf <- (uint32 OLECMDF.OLECMDF_SUPPORTED) ||| (uint32 OLECMDF.OLECMDF_ENABLED)
                 VSConstants.S_OK
             else
-                x.NextTarget.QueryStatus(&pguidCmdGroup, cCmds, prgCmds, pCmdText)            
+                x.NextTarget.QueryStatus(&pguidCmdGroup, cCmds, prgCmds, pCmdText)
+                
+    interface IDisposable with
+        member __.Dispose() = 
+            cancellationToken.Value
+            |> Option.iter (fun token -> 
+                token.Cancel()
+                token.Dispose())
+           
