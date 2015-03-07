b17d24f1d7e80d3dec949723d9be0747abb78973
dungpa <phananhdung309@yahoo.com>
2015-03-07 02:47:24 +0100

    Add documentation

--- docs/content/generatereferences.fsx
+++ docs/content/generatereferences.fsx
@@ -4,7 +4,7 @@
 #I "../../bin"
 
 (**
-Generate references in F# Interactive
+Generate references for F# Interactive
 -----------------------
 
 The feature is originally written by Tao Liu (see [the relevant article](http://apollo13cn.blogspot.dk/2012/08/f-add-reference-addon.html)).
@@ -15,7 +15,7 @@ When being integrated to VFPT, the feature has been modified as follows:
  - The `load-references.fsx` script generates `#r` directives for all references in a given project.
  - The `load-project.fsx` script generates `#load` directives for all source files in a given project.
 
-The feature can be invoked from 'Generate References in F# Interactive' command on a References node or a Project node itself (see the screenshot below).
+The feature can be invoked from 'Generate References for F# Interactive' command on a References node or a Project node itself (see the screenshot below).
  
 ![Generate references](img/generate_references.png)
 
---
+++ docs/content/navigatetosource.fsx
@@ -0,0 +1,34 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+
+(**
+Navigate to Source
+==============================
+
+When the feature is enabled and users click Go to Definition (F12), Navigate to Source happens if:
+
+ - current symbol originates from BCL libraries. The behaviour is to go to exact line in http://referencesource.microsoft.com/.
+ - current symbol comes from an assembly with associated source-indexed pdb file. The behaviour is to go to the source code provider that hosts associated source code.
+
+Here is a screencast of the feature in action:
+
+![Navigate to Source](img/navigate_to_source.gif)
+
+I am a library author, what should I do to enable source indexing on my libraries?
+-----------------------
+
+You should run [SourceLink](http://ctaggart.github.io/SourceLink/) on pdb files and distribute these files with your NuGet packages. 
+[ProjectScaffold](https://github.com/fsprojects/ProjectScaffold) also includes a SourceLink target to make the process trivial to run.
+Once the source-indexed pdb files are available, Navigate to Source should work as expected.
+
+I am a normal user, what should I do to have Navigate to Source working?
+-----------------------
+
+Navigate to Source should work for any assembly if its source-indexed pdb file locates in the symbol cache directory.
+You should enable source server support and set symbol cache directory -- see a simple guide at http://ctaggart.github.io/SourceLink/visualstudio.html. 
+During the debugging process, pdb files will be downloaded and cached in this folder.
+And the subsequent uses of these pdb files, Navigate to Source will pick them up.
+ 
+*)
