007a66105a08249c51df31a0a32b3c01f4f6ad93
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2015-07-03 17:42:24 +0300

    implement LintTagger

---
+++ src/FSharpVSPowerTools.Logic/LintTagger.fs
@@ -0,0 +1,92 @@
+﻿namespace FSharpVSPowerTools.Linting
+
+open System
+open Microsoft.VisualStudio.Text
+open Microsoft.VisualStudio.Text.Editor
+open Microsoft.VisualStudio.Text.Tagging
+open Microsoft.VisualStudio.Shell.Interop
+open FSharpVSPowerTools
+open FSharpVSPowerTools.AsyncMaybe
+open FSharpVSPowerTools.ProjectSystem
+open FSharpLint.Application
+
+type LintTag(tooltip) = inherit ErrorTag("Lint", tooltip)
+
+type LintTagger(textDocument: ITextDocument,
+                view: ITextView, 
+                vsLanguageService: VSLanguageService, 
+                serviceProvider: IServiceProvider,
+                projectFactory: ProjectFactory) as self =
+    let tagsChanged = Event<_, _>()
+    let mutable wordSpans = []
+
+    let buffer = view.TextBuffer
+
+    let updateAtCaretPosition () =
+        asyncMaybe {
+            let dte = serviceProvider.GetService<EnvDTE.DTE, SDTE>()
+            let! doc = dte.GetCurrentDocument(textDocument.FilePath)
+            let! project = projectFactory.CreateForDocument buffer doc
+            let source = buffer.CurrentSnapshot.GetText()
+            let! checkResults = vsLanguageService.ParseAndCheckFileInProject (doc.FullName, source, project) |> liftAsync
+            let! ast = checkResults.GetUntypedAst()
+            let res = 
+                Lint.lintParsedFile 
+                    { FinishEarly = None
+                      Configuration = None //Configuration.defaultConfiguration
+                      ReceivedWarning = None }
+                    { Ast = ast
+                      Source = source
+                      TypeCheckResults = checkResults.GetCheckResults() }
+                    doc.FullName
+
+            return
+                match res with
+                | LintResult.Success warnings ->
+                    warnings |> List.choose (fun warn -> 
+                        fromFSharpRange view.TextBuffer.CurrentSnapshot warn.Range
+                        |> Option.map (fun span -> warn, span))
+                | LintResult.Failure _ -> []
+        }
+        |> Async.map (fun spans -> 
+            let spans = 
+                match spans with 
+                | Some x -> x
+                | None -> []
+            wordSpans <- spans
+            let span = SnapshotSpan(buffer.CurrentSnapshot, 0, buffer.CurrentSnapshot.Length)
+            tagsChanged.Trigger(self, SnapshotSpanEventArgs span))
+        |> Async.StartInThreadPoolSafe
+
+    let docEventListener = new DocumentEventListener ([ViewChange.layoutEvent view; ViewChange.caretEvent view], 200us, 
+                                                      updateAtCaretPosition)
+
+    let getTags (spans: NormalizedSnapshotSpanCollection): ITagSpan<LintTag> list = 
+        [
+            match wordSpans with
+            | [] -> ()
+            | _ ->
+                let currentSnapshot = spans.[0].Snapshot
+                let wordSpans = 
+                    if currentSnapshot = (snd wordSpans.[0]).Snapshot then
+                        wordSpans
+                    else
+                        // If the requested snapshot isn't the same as the one our words are on, translate our spans
+                        // to the expected snapshot
+                        wordSpans |> List.map (fun (warn, span) -> warn, span.TranslateTo(currentSnapshot, SpanTrackingMode.EdgeExclusive))
+                
+                for warn, span in wordSpans do
+                    if spans.OverlapsWith span then
+                        yield upcast TagSpan<LintTag>(span, LintTag(warn.Info))
+        ]
+
+    interface ITagger<LintTag> with
+        member __.GetTags spans =
+            upcast (protectOrDefault (fun _ -> getTags spans) [])
+        
+        [<CLIEvent>]
+        member __.TagsChanged = tagsChanged.Publish
+
+    interface IDisposable with
+        member __.Dispose() = 
+            (docEventListener :> IDisposable).Dispose()
---
+++ src/FSharpVSPowerTools/LintTaggerProvider.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Shell;
+using EnvDTE;
+using FSharpVSPowerTools.HighlightUsage;
+using FSharpVSPowerTools.ProjectSystem;
+using System;
+using FSharpVSPowerTools.Linting;
+
+namespace FSharpVSPowerTools
+{
+    [Export(typeof(IViewTaggerProvider))]
+    [ContentType("F#")]
+    [TagType(typeof(LintTag))]
+    public class LintTaggerProvider : IViewTaggerProvider
+    {
+        [Import]
+        internal VSLanguageService fsharpVsLanguageService = null;
+
+        [Import]
+        internal ITextDocumentFactoryService textDocumentFactoryService = null;
+
+        [Import(typeof(SVsServiceProvider))]
+        internal IServiceProvider serviceProvider = null;
+
+        [Import]
+        internal ProjectFactory projectFactory = null;
+
+        private static readonly Type serviceType = typeof(LintTagger);
+
+        public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag
+        {
+            // Only provide highlighting on the top-level buffer
+            if (textView.TextBuffer != buffer) return null;
+
+            //var generalOptions = Setting.getGeneralOptions(serviceProvider);
+            //if (generalOptions == null || !generalOptions.HighlightUsageEnabled) return null;
+
+            ITextDocument doc;
+            if (textDocumentFactoryService.TryGetTextDocument(buffer, out doc))
+            {
+                return buffer.Properties.GetOrCreateSingletonProperty(serviceType, 
+                    () => new LintTagger(doc, textView, fsharpVsLanguageService, serviceProvider, projectFactory) as ITagger<T>);
+            }
+
+            return null;
+        }
+    }
+}
