b1da6ef4242a98326c67a6c963fbfb2cdd371482
dungpa <phananhdung309@yahoo.com>
2015-07-26 13:59:07 +0200

    Bump version again

--- src/FSharpVSPowerTools.Logic/AssemblyInfo.fs
+++ src/FSharpVSPowerTools.Logic/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("FSharpVSPowerTools.Logic")>]
 [<assembly: AssemblyProductAttribute("FSharpVSPowerTools")>]
 [<assembly: AssemblyDescriptionAttribute("A collection of additional commands for F# in Visual Studio")>]
-[<assembly: AssemblyVersionAttribute("2.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("2.0.0")>]
+[<assembly: AssemblyVersionAttribute("2.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("2.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.0.0"
+    let [<Literal>] Version = "2.1.0"
--- src/FSharpVSPowerTools/Properties/AssemblyInfo.cs
+++ src/FSharpVSPowerTools/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitleAttribute("FSharpVSPowerTools")]
 [assembly: AssemblyProductAttribute("FSharpVSPowerTools")]
 [assembly: AssemblyDescriptionAttribute("A collection of additional commands for F# in Visual Studio")]
-[assembly: AssemblyVersionAttribute("2.0.0")]
-[assembly: AssemblyFileVersionAttribute("2.0.0")]
+[assembly: AssemblyVersionAttribute("2.1.0")]
+[assembly: AssemblyFileVersionAttribute("2.1.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "2.0.0";
+        internal const string Version = "2.1.0";
     }
 }
