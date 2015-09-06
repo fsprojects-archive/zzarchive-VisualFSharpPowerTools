f1f9604dee7ef334a8e34722b85654c3a2d55c38
dungpa <phananhdung309@yahoo.com>
2015-09-06 23:54:59 +0200

    Disable multi-project tests again

--- tests/FSharpVSPowerTools.Tests/HighlightUsageTaggerTests.fs
+++ tests/FSharpVSPowerTools.Tests/HighlightUsageTaggerTests.fs
@@ -185,7 +185,7 @@ let _ = Project.GetSample()
                 |> assertEqual
                      [ (4, 6) => (4, 12); (5, 9) => (5, 15) ])
 
-    [<Test>]
+    [<Test; Category "AppVeyorLongRunning">]
     let ``should generate highlight usage tags for multi-project symbols``() = 
         let content = """
 namespace Project2
