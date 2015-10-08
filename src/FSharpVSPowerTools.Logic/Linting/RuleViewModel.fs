639720cbed1275cb7cc731669f659805e5d9793c
Matt <matt@mattmcveigh.co.uk>
2015-10-08 18:55:08 +0100

    Checkboxes for analysers select and deselect rules
    TODO: Handle third state for parent checkbox when some children are
    selected

--- src/FSharpVSPowerTools.Logic/Linting/RuleViewModel.fs
+++ src/FSharpVSPowerTools.Logic/Linting/RuleViewModel.fs
@@ -1,11 +1,37 @@
 ﻿namespace FSharpVSPowerTools.Linting
 
+open System.ComponentModel
 open FSharp.ViewModule
 
 type RuleViewModel(name:string, rules:RuleViewModel seq, settings, isChecked:bool) as this =
     inherit ViewModelBase()
 
     let isChecked = this.Factory.Backing(<@ this.IsChecked @>, isChecked)
+
+    let childCheckboxChanged _ = 
+        let ruleIsChecked (x:RuleViewModel) = x.IsChecked
+
+        let allChildrenChecked = Seq.forall ruleIsChecked
+        let noChildrenChecked = Seq.forall (ruleIsChecked >> not)
+
+        if allChildrenChecked rules then
+            this.IsChecked <- true
+        else if noChildrenChecked rules then
+            this.IsChecked <- false
+        else
+            // TODO: Third state.
+            ()
+
+    do
+        for rule in rules do
+            let notifyRuleChanged = rule :> INotifyPropertyChanged
+
+            notifyRuleChanged.PropertyChanged.Subscribe childCheckboxChanged
+            |> ignore
+
+        (this :> INotifyPropertyChanged).PropertyChanged.Subscribe
+            (fun _ -> for rule in rules do rule.IsChecked <- this.IsChecked)
+        |> ignore
             
     member __.Name = name
 
