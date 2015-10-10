c3a5c438e6690de6a50c1e01ea0fc658af04c93d
Matt <matt@mattmcveigh.co.uk>
2015-10-10 12:59:19 +0100

    Setup lint doc page

---
+++ docs/content/lint.fsx
@@ -0,0 +1,10 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+
+(**
+Lint
+-----------------------
+
+*)
