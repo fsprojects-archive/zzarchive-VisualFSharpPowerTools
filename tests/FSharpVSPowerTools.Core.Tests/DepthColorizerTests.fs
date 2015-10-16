c9d2fd72f0faa37c116436474ce1b380b81932ed
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2015-10-16 14:27:28 +0300

    fixed: DepthColorizerTests and XmlDocTests dead locked

--- tests/FSharpVSPowerTools.Core.Tests/DepthColorizerTests.fs
+++ tests/FSharpVSPowerTools.Core.Tests/DepthColorizerTests.fs
@@ -15,100 +15,104 @@ open TestHelpers.LanguageServiceTestHelper
 let fileName = Path.Combine(__SOURCE_DIRECTORY__, "DepthColorizerSampleFile.fs")
 let input = File.ReadAllText(fileName)
 let languageService = LanguageService()
-let output = async {
-    let! results = languageService.ParseFileInProject (projectOptions fileName, fileName, input)
-    return! DepthParser.getNonoverlappingDepthRanges (input, results.ParseTree) } |> Async.RunSynchronously
+let output = 
+    lazy
+        async {
+            let! results = languageService.ParseFileInProject (projectOptions fileName, fileName, input)
+            return! DepthParser.getNonoverlappingDepthRanges (input, results.ParseTree) 
+        } 
+        |> Async.RunSynchronously
 
 [<Test>]
 let ``should create single level at depth 0 for module definition``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 1 && i = 0 && c = 17 && d = 0) |> assertEqual true
-    output |> Seq.filter (fun (l, _, _, _) -> l = 1) |> Seq.length |> assertEqual 1
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 1 && i = 0 && c = 17 && d = 0) |> assertEqual true
+    output.Value |> Seq.filter (fun (l, _, _, _) -> l = 1) |> Seq.length |> assertEqual 1
 
 [<Test>]
 let ``should create single level for blank line``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 2 && i = 0 && c = 0 && d = 0) |> assertEqual true
-    output |> Seq.filter (fun (l, _, _, _) -> l = 2 ) |> Seq.length |> assertEqual 1
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 2 && i = 0 && c = 0 && d = 0) |> assertEqual true
+    output.Value |> Seq.filter (fun (l, _, _, _) -> l = 2 ) |> Seq.length |> assertEqual 1
 
 [<Test>]
 let ``should create depth 1 for module-level let``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 3 && i = 0 && c = 10 && d = 1) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 3 && i = 0 && c = 10 && d = 1) |> assertEqual true
     
 [<Test>]
 let ``should create depth 1 for local module``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 5 && i = 0 && c = 20 && d = 1) |> assertEqual true
-    output |> Seq.exists (fun (l, i, c, d) -> l = 5 && i = 0 && c = 0 && d = 0) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 5 && i = 0 && c = 20 && d = 1) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 5 && i = 0 && c = 0 && d = 0) |> assertEqual true
     
 [<Test>]
 let ``should increase depth for inner module``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 6 && i = 0 && c = 4 && d = 1) |> assertEqual true
-    output |> Seq.exists (fun (l, i, c, d) -> l = 6 && i = 4 && c = 18 && d = 2) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 6 && i = 0 && c = 4 && d = 1) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 6 && i = 4 && c = 18 && d = 2) |> assertEqual true
 
 [<Test>]
 let ``should show all lines of func at same depth``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 8 && i = 8 && c = 255 && d = -3) |> assertEqual true
-    output |> Seq.exists (fun (l, i, c, d) -> l = 9 && i = 12 && c = 13 && d = 3) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 8 && i = 8 && c = 255 && d = -3) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 9 && i = 12 && c = 13 && d = 3) |> assertEqual true
     
 [<Test>]
 let ``should increase depth for type definition``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 12 && i = 8 && c = 255 && d = -3) |> assertEqual true
-    output |> Seq.exists (fun (l, i, c, d) -> l = 13 && i = 12 && c = 17 && d = 3) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 12 && i = 8 && c = 255 && d = -3) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 13 && i = 12 && c = 17 && d = 3) |> assertEqual true
     
 [<Test>]
 let ``should decrease depth for end of type definition``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 13 && i = 17 && c = 21 && d = 2) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 13 && i = 17 && c = 21 && d = 2) |> assertEqual true
     
 [<Test>]
 let ``should increase depth for body of if statement``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 27 && i = 16 && c = 52 && d = 3) |> assertEqual true
-    output |> Seq.exists (fun (l, i, c, d) -> l = 29 && i = 16 && c = 26 && d = 3) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 27 && i = 16 && c = 52 && d = 3) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 29 && i = 16 && c = 26 && d = 3) |> assertEqual true
 
 [<Test>]
 let ``should increase depth for type member``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 73 && i = 8 && c = 18 && d = 3) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 73 && i = 8 && c = 18 && d = 3) |> assertEqual true
 
 [<Test>]
 let ``should increase depth for interface member``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 81 && i = 8 && c = 255 && d = -3) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 81 && i = 8 && c = 255 && d = -3) |> assertEqual true
 
 [<Test>]
 let ``should increase depth for elif``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 150 && i = 12 && c = 17 && d = 5) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 150 && i = 12 && c = 17 && d = 5) |> assertEqual true
 
 [<Test>]
 let ``should increase depth for nested elif``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 152 && i = 12 && c = 17 && d = 6) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 152 && i = 12 && c = 17 && d = 6) |> assertEqual true
 
 [<Test>]
 let ``should increase depth inside try block``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 131 && i = 12 && c = 17 && d = 4) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 131 && i = 12 && c = 17 && d = 4) |> assertEqual true
 
 [<Test>]
 let ``should increase depth inside with block``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 133 && i = 12 && c = 17 && d = 4) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 133 && i = 12 && c = 17 && d = 4) |> assertEqual true
 
 [<Test>]
 let ``should increase depth inside finally block``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 138 && i = 12 && c = 17 && d = 4) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 138 && i = 12 && c = 17 && d = 4) |> assertEqual true
 
 [<Test>]
 let ``should increase depth inside else``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 128 && i = 12 && c = 17 && d = 4) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 128 && i = 12 && c = 17 && d = 4) |> assertEqual true
 
 [<Test>]
 let ``should increase depth inside matched pattern``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 143 && i = 8 && c = 20 && d = 4) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 143 && i = 8 && c = 20 && d = 4) |> assertEqual true
 
 [<Test>]
 let ``should increase depth inside while loop``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 157 && i = 12 && c = 17 && d = 4) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 157 && i = 12 && c = 17 && d = 4) |> assertEqual true
 
 [<Test>]
 let ``should increase depth inside for loop``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 160 && i = 12 && c = 17 && d = 4) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 160 && i = 12 && c = 17 && d = 4) |> assertEqual true
 
 [<Test>]
 let ``should increase depth inside async expression``() =
-    output |> Seq.exists (fun (l, i, c, d) -> l = 175 && i = 12 && c = 17 && d = 4) |> assertEqual true
+    output.Value |> Seq.exists (fun (l, i, c, d) -> l = 175 && i = 12 && c = 17 && d = 4) |> assertEqual true
 
 #if INTERACTIVE
 Seq.iter (printfn "%A") output;;
--- tests/FSharpVSPowerTools.Core.Tests/XmlDocTests.fs
+++ tests/FSharpVSPowerTools.Core.Tests/XmlDocTests.fs
@@ -15,47 +15,51 @@ open TestHelpers.LanguageServiceTestHelper
 let fileName = Path.Combine(__SOURCE_DIRECTORY__, "SampleFile.fs")
 let input = File.ReadAllText(fileName)
 let languageService = LanguageService()
-let output = async {
-    let! parseResults = languageService.ParseFileInProject (projectOptions fileName, fileName, input)
-    return! XmlDocParser.getXmlDocables (input, parseResults.ParseTree) } |> Async.RunSynchronously |> Set.ofList
+let output = 
+    lazy
+        async {
+            let! parseResults = languageService.ParseFileInProject (projectOptions fileName, fileName, input)
+            return! XmlDocParser.getXmlDocables (input, parseResults.ParseTree) 
+        } 
+        |> Async.RunSynchronously |> Set.ofList
 
 [<Test>]
 let ``should create XML Doc for module-level let bounds``() =
-    Set.contains (XmlDocable(3, 0, [])) output |> assertEqual true
-    Set.contains (XmlDocable(5, 0, ["x"; "y"])) output |> assertEqual true
-    Set.contains (XmlDocable(7, 0, ["x"; "y"])) output |> assertEqual true
-    Set.contains (XmlDocable(77, 0, ["x"])) output |> assertEqual true
+    Set.contains (XmlDocable(3, 0, [])) output.Value |> assertEqual true
+    Set.contains (XmlDocable(5, 0, ["x"; "y"])) output.Value |> assertEqual true
+    Set.contains (XmlDocable(7, 0, ["x"; "y"])) output.Value |> assertEqual true
+    Set.contains (XmlDocable(77, 0, ["x"])) output.Value |> assertEqual true
 
 [<Test>]
 let ``should not create XML Doc for module-level let bounds which already have non-empty XML Docs``() =
-    Set.contains (XmlDocable(79, 0, ["x"])) output |> assertEqual false
-    Set.contains (XmlDocable(82, 0, ["x"])) output |> assertEqual false
+    Set.contains (XmlDocable(79, 0, ["x"])) output.Value |> assertEqual false
+    Set.contains (XmlDocable(82, 0, ["x"])) output.Value |> assertEqual false
 
 [<Test>]
 let ``should create XML Doc for type names``() =
-    Set.contains (XmlDocable(18, 0, [])) output |> assertEqual true
-    Set.contains (XmlDocable(42, 0, [])) output |> assertEqual true
-    Set.contains (XmlDocable(45, 0, [])) output |> assertEqual true
-    Set.contains (XmlDocable(86, 0, [])) output |> assertEqual true
+    Set.contains (XmlDocable(18, 0, [])) output.Value |> assertEqual true
+    Set.contains (XmlDocable(42, 0, [])) output.Value |> assertEqual true
+    Set.contains (XmlDocable(45, 0, [])) output.Value |> assertEqual true
+    Set.contains (XmlDocable(86, 0, [])) output.Value |> assertEqual true
 
 [<Test>]
 let ``should not create XML Doc for types which already have non-empty XML Docs``() =
-    Set.contains (XmlDocable(89, 0, [])) output |> assertEqual false
+    Set.contains (XmlDocable(89, 0, [])) output.Value |> assertEqual false
 
 [<Test>]
 let ``should create XML Doc for members``() =
-    Set.contains (XmlDocable(24, 4, ["z"; "x"; "y"])) output |> assertEqual true
-    Set.contains (XmlDocable(27, 4, [])) output |> assertEqual true
-    Set.contains (XmlDocable(33, 4, ["x"; "y"])) output |> assertEqual true
-    Set.contains (XmlDocable(91, 4, [])) output |> assertEqual true
+    Set.contains (XmlDocable(24, 4, ["z"; "x"; "y"])) output.Value |> assertEqual true
+    Set.contains (XmlDocable(27, 4, [])) output.Value |> assertEqual true
+    Set.contains (XmlDocable(33, 4, ["x"; "y"])) output.Value |> assertEqual true
+    Set.contains (XmlDocable(91, 4, [])) output.Value |> assertEqual true
 
 [<Test>]
 let ``should create XML Doc for let bindings of anonymous functions``() =
-    Set.contains (XmlDocable(97, 0, [ "x"; "y"])) output |> assertEqual true
+    Set.contains (XmlDocable(97, 0, [ "x"; "y"])) output.Value |> assertEqual true
 
 [<Test>]
 let ``should not create XML Doc for members which already have non-empty XML Docs``() =
-    Set.contains (XmlDocable(93, 4, [])) output |> assertEqual false
+    Set.contains (XmlDocable(93, 4, [])) output.Value |> assertEqual false
 
 [<TestCase ("///<", 3)>]
 [<TestCase ("/// <", 4)>]
