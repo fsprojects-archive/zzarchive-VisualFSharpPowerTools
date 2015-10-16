f10f14fd10f2928515abbd3e1931091b86883399
Jared Hester <jared.hester@gmail.com>
2015-10-16 13:04:42 -0400

    F# Implementation of OutliningTaggerProvider
    renamed Tagger to OutliningTagger
    pared down the implementation
    plus a few more comments

--- src/FSharpVSPowerTools.Logic/Outlining/OutliningTagger.fs
+++ src/FSharpVSPowerTools.Logic/Outlining/OutliningTagger.fs
@@ -3,6 +3,11 @@
 open System
 open Microsoft.VisualStudio.Text
 open Microsoft.VisualStudio.Text.Tagging
+open Microsoft.VisualStudio.Text.Editor
+open Microsoft.VisualStudio.Text.Operations
+open Microsoft.VisualStudio.Utilities
+open Microsoft.VisualStudio.Shell
+open System.ComponentModel.Composition
 open FSharpVSPowerTools
 open FSharpVSPowerTools.Utils
 open FSharpVSPowerTools.ProjectSystem
@@ -19,64 +24,53 @@ let [<Literal>] private MaxTooltipLines = 25
 
 type ScopedSpan = Scope * SnapshotSpan
 
-type Tagger
-    (buffer: ITextBuffer,
-     textDocument: ITextDocument,
+type OutliningTagger
+    (textDocument: ITextDocument,
      serviceProvider : IServiceProvider,
      projectFactory: ProjectFactory,
      languageService: VSLanguageService) as self = 
 
-    let tagsChanged = Event<_,_>()
-    
+    let buffer = textDocument.TextBuffer
+    let tagsChanged = Event<_,_> ()
     let mutable snapshotSpans : ScopedSpan [] = [||]
 
-
-    // triggerUpdate => tagsChanged
+    /// triggerUpdate -=> tagsChanged
     let triggerUpdate newSnapshotSpans =
         snapshotSpans <- newSnapshotSpans
         tagsChanged.Trigger(
-            self,
-            SnapshotSpanEventArgs(
-                SnapshotSpan(
-                    buffer.CurrentSnapshot,
-                    0,
-                    buffer.CurrentSnapshot.Length - 1)))
+            self, SnapshotSpanEventArgs (SnapshotSpan   ( buffer.CurrentSnapshot
+                                                        , 0
+                                                        , buffer.CurrentSnapshot.Length - 1)))
 
-    let fromFSharpRange (snapshot: ITextSnapshot) (sr: srange) = 
+
+    /// convert the FSharp compiler range in SRanges into a snapshotspan and tuple it with its Scope tag
+    let fromSRange (snapshot: ITextSnapshot) (sr: srange) : ScopedSpan option = 
         let r = sr.Range
-        match fromRange snapshot (r.StartLine, r.StartColumn, r.EndLine, r.EndColumn) with
+        match VSUtils.fromRange snapshot (r.StartLine, r.StartColumn, r.EndLine, r.EndColumn) with
         | Some sshot -> Some (sr.Scope,sshot)
         | None       -> None
 
-    // doUpdate => triggerUpdate => tagsChanged
+
+    /// doUpdate -=> triggerUpdate -=> tagsChanged
     let doUpdate () =
         let uiContext = SynchronizationContext.Current
         asyncMaybe {
             let dte = serviceProvider.GetService<EnvDTE.DTE, SDTE>()
-            let snapshot = buffer.CurrentSnapshot
-            let source = snapshot.GetText()
-            let! doc = dte.GetCurrentDocument(textDocument.FilePath)
+            let snapshot = buffer.CurrentSnapshot in let source = snapshot.GetText ()
+            let! doc = dte.GetCurrentDocument (textDocument.FilePath)
             let! project = projectFactory.CreateForDocument buffer doc
             let! parseFileResults = languageService.ParseFileInProject (doc.FullName, source, project) |> AsyncMaybe.liftAsync
             let! ast = parseFileResults.ParseTree
-            let ranges =
-                ast
-                |> getOutliningRanges 
-                |> Seq.map (fun r -> fromFSharpRange snapshot r)
-                |> Seq.choose id
-                |> Array.ofSeq
-
+            let ranges = (getOutliningRanges>>Seq.choose (fromSRange snapshot)>>Array.ofSeq) ast
             do! Async.SwitchToContext uiContext |> AsyncMaybe.liftAsync
             triggerUpdate ranges
-
         } |> Async.Ignore |> Async.StartInThreadPoolSafe
 
-    // viewUpdate => doUpdate => triggerUpdate => tagsChanged
+
+    /// viewUpdate -=> doUpdate -=> triggerUpdate -=> tagsChanged
     let docEventListener =
-        new DocumentEventListener(
-            [ViewChange.bufferEvent buffer],
-            UpdateDelay,
-            doUpdate)  :> IDisposable
+        new DocumentEventListener ([ViewChange.bufferEvent buffer], UpdateDelay, doUpdate) :> IDisposable
+
 
     // drills down into the snapshot text to find the first non whitespace line 
     // to display as the text inside the collapse box preceding the `...`
@@ -86,86 +80,102 @@ type Tagger
         let rec loop acc =
             if acc >= snapshot.LineCount + firstLineNum then "" else
             let text =  if acc = firstLineNum then
-                            let _,colstart,_,_ = snapshotSpan.ToRange()
+                            let _,colstart,_,_ = snapshotSpan.ToRange ()
                             snapshot.GetLineFromLineNumber(acc).GetText().Substring(colstart).Trim()
                         else snapshot.GetLineFromLineNumber(acc).GetText().Trim()
             if String.IsNullOrWhiteSpace text then loop (acc+1) else text
         loop firstLineNum
 
-    
+    /// Find the length of the shortest whitespace indentation in the textblock used for the outlining
+    /// hint tooltip, then trim that length from the front of every line in the textblock
     let cleanHintText (text:string) =
         let leadingWhitespace (str:string) =
-            let charr = str.ToCharArray()
+            let charr = str.ToCharArray ()
             let rec loop acc  =
                 if acc >= charr.Length then acc 
-                elif not (Char.IsWhiteSpace charr.[acc]) then acc
-                else loop (acc+1)
-            loop 0
+                elif not (Char.IsWhiteSpace charr.[acc]) then acc else loop (acc+1) in loop 0
         let lines = text.Split [|'\n'|]
         let minlead = 
             let seed = if lines = [||] then 0 else lines.[0] |> leadingWhitespace
             (seed, lines) ||> Array.fold (fun acc elm -> leadingWhitespace elm |> min acc)
-        (StringBuilder(), lines) ||> Array.fold (fun acc elm -> elm.Substring minlead |> acc.Append )
-            |> string 
+        (StringBuilder (), lines) ||> Array.fold (fun acc elm -> elm.Substring minlead |> acc.Append ) |> string 
 
-    let createTagSpan ((scope,snapshotSpan): Scope*SnapshotSpan) =
+
+    let createTagSpan ((scope,snapshotSpan): ScopedSpan) =
         try
-            let snapshot = snapshotSpan.Snapshot
-            let firstLine = snapshot.GetLineFromPosition(snapshotSpan.Start.Position)
-            let mutable lastLine = snapshot.GetLineFromPosition(snapshotSpan.End.Position)
+            let snapshot = snapshotSpan.Snapshot in let firstLine = snapshot.GetLineFromPosition (snapshotSpan.Start.Position)
+            let mutable lastLine = snapshot.GetLineFromPosition (snapshotSpan.End.Position)
 
             let nHintLines = lastLine.LineNumber - firstLine.LineNumber + 1
             if nHintLines > MaxTooltipLines then
-                lastLine <- snapshot.GetLineFromLineNumber(firstLine.LineNumber + MaxTooltipLines - 1)
+                lastLine <- snapshot.GetLineFromLineNumber (firstLine.LineNumber + MaxTooltipLines - 1)
 
-            let missingLinesCount = Math.Max(nHintLines - MaxTooltipLines, 0)
+            let missingLinesCount = Math.Max (nHintLines - MaxTooltipLines, 0)
 
-            let hintSnapshotSpan = SnapshotSpan(firstLine.Start, lastLine.End)
+            let hintSnapshotSpan = SnapshotSpan (firstLine.Start, lastLine.End)
             let collapseText, collapseSpan = 
                 match scope with
-                | Scope.Same ->
-                    (getHintText snapshotSpan)+"...", snapshotSpan
-                | _ (* Scope.Below *) ->
-                     "...", SnapshotSpan(firstLine.End, snapshotSpan.End)
-            TagSpan(
-                collapseSpan,
-                { new IOutliningRegionTag with
-                    member __.CollapsedForm      = collapseText :> obj
-                    member __.IsDefaultCollapsed = false
-                    member __.IsImplementation   = false
-                    member __.CollapsedHintForm  =
-                        let text = hintSnapshotSpan.GetText() |> cleanHintText
-                        match missingLinesCount with
-                        | 0 -> text :> obj
-                        | n -> sprintf "%s\n...\n\n +%d lines" text n :> obj
+                | Scope.Same -> ((getHintText snapshotSpan) + "...", snapshotSpan)
+                | _ (* Scope.Below *) -> ("...", SnapshotSpan (firstLine.End, snapshotSpan.End))
+            TagSpan ( collapseSpan,
+                    { new IOutliningRegionTag with
+                        member __.CollapsedForm      = collapseText :> obj
+                        member __.IsDefaultCollapsed = false
+                        member __.IsImplementation   = false
+                        member __.CollapsedHintForm  =
+                            let text = hintSnapshotSpan.GetText () |> cleanHintText
+                            match missingLinesCount with
+                            | 0 -> text :> obj
+                            | n -> sprintf "%s\n\n +%d lines..." text n :> obj
                     }) :> ITagSpan<_>
-        with
-            | :? ArgumentOutOfRangeException ->
+        with | :? ArgumentOutOfRangeException ->
                 Logging.logInfo "ArgumentOutOfRangeException in Outlining.Tagger.createTagSpan"
                 null
 
+
+    /// viewUpdate -=> doUpdate -=> triggerUpdate -=> tagsChanged -=> getTags
     let getTags (nssc: NormalizedSnapshotSpanCollection) =
-        if Seq.isEmpty nssc || Array.isEmpty snapshotSpans then Seq.empty
-        else
-            let newSnapshot = (Seq.head nssc).Snapshot
-            if newSnapshot.Version <> (snd snapshotSpans.[0]).Snapshot.Version then
-                snapshotSpans <- snapshotSpans 
-                                |> Array.map (fun (scp,spn) -> 
-                                    scp,spn.TranslateTo(newSnapshot, SpanTrackingMode.EdgeExclusive))
-            snapshotSpans
-            |> Seq.filter (snd>>nssc.IntersectsWith)
-            |> Seq.map createTagSpan
+        if Seq.isEmpty nssc || Array.isEmpty snapshotSpans then Seq.empty else
+        let newSnapshot = (Seq.head nssc).Snapshot
+        if newSnapshot.Version <> (snd snapshotSpans.[0]).Snapshot.Version then
+            snapshotSpans <- snapshotSpans 
+                            |> Array.map (fun (scp,spn) -> scp,spn.TranslateTo  ( newSnapshot
+                                                                                , SpanTrackingMode.EdgeExclusive))            
+        snapshotSpans |> (Seq.filter (snd>>nssc.IntersectsWith)>>(Seq.map createTagSpan))
 
+    // Try to construct tags on creation
+    do doUpdate ()
 
-    do doUpdate()
 
     interface ITagger<IOutliningRegionTag> with
-
         member __.GetTags spans = getTags spans
-
-        [<CLIEvent>]
-        member __.TagsChanged : IEvent<_,_> = tagsChanged.Publish
+        [<CLIEvent>] member __.TagsChanged : IEvent<_,_> = tagsChanged.Publish
 
     interface IDisposable with
-        member __.Dispose() =
-            docEventListener.Dispose()
+        member __.Dispose() = docEventListener.Dispose ()
+
+
+[<  Export (typeof<ITaggerProvider>)
+;   ContentType "F#"
+;   TagType (typeof<IOutliningRegionTag>) >]
+type OutliningTaggerProvider [<ImportingConstructor>] 
+    ( serviceProvider: IServiceProvider
+    , textDocumentFactoryService : ITextDocumentFactoryService
+    , projectFactory: ProjectFactory
+    , vsLanguageService : VSLanguageService
+    ) as self =
+
+    member __.CreateTagger buffer : ITagger<IOutliningRegionTag> = 
+        let doc = ref (Unchecked.defaultof<ITextDocument>)
+        if textDocumentFactoryService.TryGetTextDocument (buffer, doc) then
+            new OutliningTagger ( !doc
+                                , serviceProvider
+                                , projectFactory
+                                , vsLanguageService ) :> ITagger<_>          
+        else null
+
+    interface ITaggerProvider with
+        member __.CreateTagger buffer : ITagger<_> = 
+            self.CreateTagger buffer :> obj :?> _
+            
+   
