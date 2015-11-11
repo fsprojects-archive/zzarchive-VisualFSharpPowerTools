f2dccef39da9879116fa40b73a81a49796c62ac5
Jared Hester <jared.hester@gmail.com>
2015-11-11 13:30:28 -0500

    reorganized files

--- src/FSharpVSPowerTools.Pure/PowerToolsCommandsPackage.fs
+++ src/FSharpVSPowerTools.Pure/PowerToolsCommandsPackage.fs
@@ -19,200 +19,200 @@ open FSharpVSPowerTools.Reference
 
 module private useless = ()
 
-//[<PackageRegistration(UseManagedResourcesOnly = true)>]
-//[<ProvideMenuResource(resourceID= "Menus.ctmenu", version=1)>]
-//[<InstalledProductRegistration("#110", "#112", AssemblyVersionInformation.Version, IconResourceID = 400)>]
-//[<ProvideBindingPath>]
-////[<ProvideOptionPage(typeof<GeneralOptionsPage>, Resource.vsPackageTitle, "General", categoryResourceID= 0, pageNameResourceID= 0, supportsAutomation= true, keywordListResourceId= 0)>]
-////[<ProvideOptionPage(typeof<FantomasOptionsPage>, Resource.vsPackageTitle, "Formatting", categoryResourceID= 0, pageNameResourceID= 0, supportsAutomation= true, keywordListResourceId= 0)>]
-////[<ProvideOptionPage(typeof<CodeGenerationOptionsPage>, Resource.vsPackageTitle, "Code Generation", categoryResourceID= 0, pageNameResourceID= 0, supportsAutomation= true, keywordListResourceId= 0)>]
-////[<ProvideOptionPage(typeof<GlobalOptionsPage>, Resource.vsPackageTitle, "Configuration", categoryResourceID= 0, pageNameResourceID= 0, supportsAutomation= true, keywordListResourceId= 0)>]
-////[<ProvideOptionPage(typeof<Linting.LintOptionsPage>, Resource.vsPackageTitle, "Lint", categoryResourceID= 0, pageNameResourceID= 0, supportsAutomation= true, keywordListResourceId= 0)>]
-////[<ProvideOptionPage(typeof<OutliningOptionsPage>, Resource.vsPackageTitle, "Outlining", categoryResourceID= 0, pageNameResourceID= 0, supportsAutomation= true, keywordListResourceId= 0)>]
-////[< ProvideService (typeof<IGeneralOptions>)>]   
-////[< ProvideService (typeof<IFormattingOptions>)>]
-////[< ProvideService (typeof<ICodeGenerationOptions>)>]
-////[< ProvideService (typeof<IGlobalOptions>)>]
-////[< ProvideService (typeof<ILintOptions>)>]
-//[< Guid "1F699E38-7D87-44F4-BC08-6B1DD5A6F926">]
-//[< ProvideAutoLoad (VSConstants.UICONTEXT.SolutionExists_string)>]
-//[< ProvideAutoLoad (VSConstants.UICONTEXT.FSharpProject_string)>]
-//[< ProvideAutoLoad (VSConstants.UICONTEXT.NoSolution_string)>]
-//type PowerToolsCommandsPackage () as self =
-//    inherit Package()
-//
-//    let mutable  pctCookie = 0u
-//    let mutable  objectManagerCookie = 0u
-//    let mutable  library = Unchecked.defaultof<FSharpLibrary>
-//
-//    static let DTE = 
-//        Lazy<DTE2> (fun () -> ServiceProvider.GlobalProvider.GetService<DTE2,DTE> ())
-//
-//    member __.GetDialogPage<'a>() = base.GetDialogPage(typeof<'a>)
-//    
-//    member __.GetService<'IVs,'SVs>() = self.GetService(typeof<'SVs>) :?> 'IVs
-//
-//    override __.Initialize () =
-//        base.Initialize ()
-//        VSUtils.ForegroundThreadGuard.BindThread ()
+[<PackageRegistration(UseManagedResourcesOnly = true)>]
+[<ProvideMenuResource(resourceID= "Menus.ctmenu", version=1)>]
+[<InstalledProductRegistration("#110", "#112", AssemblyVersionInformation.Version, IconResourceID = 400)>]
+[<ProvideBindingPath>]
+//[<ProvideOptionPage(typeof<GeneralOptionsPage>, Resource.vsPackageTitle, "General", categoryResourceID= 0, pageNameResourceID= 0, supportsAutomation= true, keywordListResourceId= 0)>]
+//[<ProvideOptionPage(typeof<FantomasOptionsPage>, Resource.vsPackageTitle, "Formatting", categoryResourceID= 0, pageNameResourceID= 0, supportsAutomation= true, keywordListResourceId= 0)>]
+//[<ProvideOptionPage(typeof<CodeGenerationOptionsPage>, Resource.vsPackageTitle, "Code Generation", categoryResourceID= 0, pageNameResourceID= 0, supportsAutomation= true, keywordListResourceId= 0)>]
+//[<ProvideOptionPage(typeof<GlobalOptionsPage>, Resource.vsPackageTitle, "Configuration", categoryResourceID= 0, pageNameResourceID= 0, supportsAutomation= true, keywordListResourceId= 0)>]
+//[<ProvideOptionPage(typeof<Linting.LintOptionsPage>, Resource.vsPackageTitle, "Lint", categoryResourceID= 0, pageNameResourceID= 0, supportsAutomation= true, keywordListResourceId= 0)>]
+//[<ProvideOptionPage(typeof<OutliningOptionsPage>, Resource.vsPackageTitle, "Outlining", categoryResourceID= 0, pageNameResourceID= 0, supportsAutomation= true, keywordListResourceId= 0)>]
+//[< ProvideService (typeof<IGeneralOptions>)>]   
+//[< ProvideService (typeof<IFormattingOptions>)>]
+//[< ProvideService (typeof<ICodeGenerationOptions>)>]
+//[< ProvideService (typeof<IGlobalOptions>)>]
+//[< ProvideService (typeof<ILintOptions>)>]
+[< Guid "1F699E38-7D87-44F4-BC08-6B1DD5A6F926">]
+[< ProvideAutoLoad (VSConstants.UICONTEXT.SolutionExists_string)>]
+[< ProvideAutoLoad (VSConstants.UICONTEXT.FSharpProject_string)>]
+[< ProvideAutoLoad (VSConstants.UICONTEXT.NoSolution_string)>]
+type PowerToolsCommandsPackage () as self =
+    inherit Package()
+
+    let mutable  pctCookie = 0u
+    let mutable  objectManagerCookie = 0u
+    let mutable  library = Unchecked.defaultof<FSharpLibrary>
+
+    static let DTE = 
+        Lazy<DTE2> (fun () -> ServiceProvider.GlobalProvider.GetService<DTE2,DTE> ())
+
+    member __.GetDialogPage<'a>() = base.GetDialogPage(typeof<'a>)
+    
+    member __.GetService<'IVs,'SVs>() = self.GetService(typeof<'SVs>) :?> 'IVs
+
+    override __.Initialize () =
+        base.Initialize ()
+        VSUtils.ForegroundThreadGuard.BindThread ()
+        
+        let serviceContainer = self :> IServiceContainer
+//        serviceContainer.AddService<IGeneralOptions> 
+//            (self.GetDialogPage<IGeneralOptions>()
+//            
+//            )
 //        
-//        let serviceContainer = self :> IServiceContainer
-////        serviceContainer.AddService<IGeneralOptions> 
-////            (self.GetDialogPage<IGeneralOptions>()
-////            
-////            )
-////        
-//        //let generalOptions 
-//        library <- FSharpLibrary Constants.guidSymbolLibrary
-//        library.LibraryCapabilities <-  Enum.Parse( typedefof<_LIB_FLAGS2>, _LIB_FLAGS.LF_PROJECT.ToString()) :?> _LIB_FLAGS2
-//       // library.LibraryCapabilities <-   asEnum<_LIB_FLAGS2>  _LIB_FLAGS.LF_PROJECT
+        //let generalOptions 
+        library <- FSharpLibrary Constants.guidSymbolLibrary
+        library.LibraryCapabilities <-  Enum.Parse( typedefof<_LIB_FLAGS2>, _LIB_FLAGS.LF_PROJECT.ToString()) :?> _LIB_FLAGS2
+       // library.LibraryCapabilities <-   asEnum<_LIB_FLAGS2>  _LIB_FLAGS.LF_PROJECT
+
+        self.RegisterLibrary ()
+
+
+
+
+    member private __.RegisterLibrary () =
+        if objectManagerCookie = 0u then
+            let objManager = self.TryGetService<IVsObjectManager2, SVsObjectManager>()
+            match  objManager with
+            | None -> () 
+            | Some objManager -> ErrorHandler.ThrowOnFailure 
+                                    (objManager.RegisterSimpleLibrary (library, &objectManagerCookie)) |> ignore
+
+    member private __.UnregisterLibrary () =
+        if objectManagerCookie <> 0u then
+            let objManager = self.TryGetService<IVsObjectManager2, SVsObjectManager>()
+            match  objManager with
+            | None -> () 
+            | Some objManager -> ErrorHandler.ThrowOnFailure 
+                                    (objManager.RegisterSimpleLibrary (library, &objectManagerCookie)) |> ignore
+
+    interface IDisposable with
+        member x.Dispose(): unit = 
+        //    UnregisterPriorityCommandTarget()
+            self.UnregisterLibrary()
+//            if (taskListCommentManager != null)
+//                (taskListCommentManager as IDisposable).Dispose()
+//            if (fsiReferenceMenu != null)
+//                (fsiReferenceMenu as IDisposable).Dispose()
+//        }
 //
-//        self.RegisterLibrary ()
-//
-//
-//
-//
-//    member private __.RegisterLibrary () =
-//        if objectManagerCookie = 0u then
-//            let objManager = self.TryGetService<IVsObjectManager2, SVsObjectManager>()
-//            match  objManager with
-//            | None -> () 
-//            | Some objManager -> ErrorHandler.ThrowOnFailure 
-//                                    (objManager.RegisterSimpleLibrary (library, &objectManagerCookie)) |> ignore
-//
-//    member private __.UnregisterLibrary () =
-//        if objectManagerCookie <> 0u then
-//            let objManager = self.TryGetService<IVsObjectManager2, SVsObjectManager>()
-//            match  objManager with
-//            | None -> () 
-//            | Some objManager -> ErrorHandler.ThrowOnFailure 
-//                                    (objManager.RegisterSimpleLibrary (library, &objectManagerCookie)) |> ignore
-//
-//    interface IDisposable with
-//        member x.Dispose(): unit = 
-//        //    UnregisterPriorityCommandTarget();
-//            self.UnregisterLibrary()
-////            if (taskListCommentManager != null)
-////                (taskListCommentManager as IDisposable).Dispose();
-////            if (fsiReferenceMenu != null)
-////                (fsiReferenceMenu as IDisposable).Dispose();
-////        }
-////
 
 //    public class PowerToolsCommandsPackage : Package, IDisposable
 //    {
-//        private FolderMenuCommands newFolderMenu;
-//        private FsiReferenceCommand fsiReferenceMenu;
-//        private FSharpLibrary library;
+//        private FolderMenuCommands newFolderMenu
+//        private FsiReferenceCommand fsiReferenceMenu
+//        private FSharpLibrary library
 //
-//        private uint pctCookie;
-//        private uint objectManagerCookie;
+//        private uint pctCookie
+//        private uint objectManagerCookie
 //        
 //        internal static Lazy<DTE2> DTE
-//            = new Lazy<DTE2>(() => ServiceProvider.GlobalProvider.GetService(typeof(DTE)) as DTE2);
+//            = new Lazy<DTE2>(() => ServiceProvider.GlobalProvider.GetService(typeof(DTE)) as DTE2)
 //
-//        internal CrossSolutionTaskListCommentManager taskListCommentManager;
+//        internal CrossSolutionTaskListCommentManager taskListCommentManager
 //
 //        protected override void Initialize()
 //        {
-//            base.Initialize();
-//            VSUtils.ForegroundThreadGuard.BindThread();
+//            base.Initialize()
+//            VSUtils.ForegroundThreadGuard.BindThread()
 //
-//            IServiceContainer serviceContainer = this;
+//            IServiceContainer serviceContainer = this
 //            serviceContainer.AddService(typeof(IGeneralOptions),
-//                delegate { return GetDialogPage(typeof(GeneralOptionsPage)); }, promote: true);
+//                delegate { return GetDialogPage(typeof(GeneralOptionsPage)) }, promote: true)
 //
 //            serviceContainer.AddService(typeof(IFormattingOptions),
-//                delegate { return GetDialogPage(typeof(FantomasOptionsPage)); }, promote: true);
+//                delegate { return GetDialogPage(typeof(FantomasOptionsPage)) }, promote: true)
 //
 //            serviceContainer.AddService(typeof(ICodeGenerationOptions),
-//                delegate { return GetDialogPage(typeof(CodeGenerationOptionsPage)); }, promote: true);
+//                delegate { return GetDialogPage(typeof(CodeGenerationOptionsPage)) }, promote: true)
 //
 //            serviceContainer.AddService(typeof(IGlobalOptions),
-//                delegate { return GetDialogPage(typeof(GlobalOptionsPage)); }, promote: true);
+//                delegate { return GetDialogPage(typeof(GlobalOptionsPage)) }, promote: true)
 //
 //            serviceContainer.AddService(typeof(ILintOptions),
-//                delegate { return GetDialogPage(typeof(Linting.LintOptionsPage)); }, promote: true);
+//                delegate { return GetDialogPage(typeof(Linting.LintOptionsPage)) }, promote: true)
 //
 //            serviceContainer.AddService(typeof(IOutliningOptions),
-//                delegate { return GetDialogPage(typeof(OutliningOptionsPage)); }, promote: true);
+//                delegate { return GetDialogPage(typeof(OutliningOptionsPage)) }, promote: true)
 //
-//            var generalOptions = GetService(typeof(IGeneralOptions)) as IGeneralOptions;
-//            PerformRegistrations(generalOptions);
+//            var generalOptions = GetService(typeof(IGeneralOptions)) as IGeneralOptions
+//            PerformRegistrations(generalOptions)
 //
-//            library = new FSharpLibrary(Constants.guidSymbolLibrary);
-//            library.LibraryCapabilities = (_LIB_FLAGS2)_LIB_FLAGS.LF_PROJECT;
+//            library = new FSharpLibrary(Constants.guidSymbolLibrary)
+//            library.LibraryCapabilities = (_LIB_FLAGS2)_LIB_FLAGS.LF_PROJECT
 //
-//            RegisterLibrary();
+//            RegisterLibrary()
 //        }
 //
 //        private void PerformRegistrations(IGeneralOptions generalOptions)
 //        {
 //            if (generalOptions.FolderOrganizationEnabled)
 //            {
-//                SetupFolderMenu();
-//                RegisterPriorityCommandTarget();
+//                SetupFolderMenu()
+//                RegisterPriorityCommandTarget()
 //            }
 //
 //            if (generalOptions.GenerateReferencesEnabled)
 //            {
-//                SetupReferenceMenu();
+//                SetupReferenceMenu()
 //            }
 //
 //            if (generalOptions.TaskListCommentsEnabled)
 //            {
 //                try
 //                {
-//                    var componentModel = GetService(typeof(SComponentModel)) as IComponentModel;
-//                    taskListCommentManager = componentModel.DefaultExportProvider.GetExportedValue<CrossSolutionTaskListCommentManager>();
-//                    Debug.Assert(taskListCommentManager != null, "This instance should have been MEF exported.");
-//                    taskListCommentManager.Activate();
+//                    var componentModel = GetService(typeof(SComponentModel)) as IComponentModel
+//                    taskListCommentManager = componentModel.DefaultExportProvider.GetExportedValue<CrossSolutionTaskListCommentManager>()
+//                    Debug.Assert(taskListCommentManager != null, "This instance should have been MEF exported.")
+//                    taskListCommentManager.Activate()
 //                }
 //                catch (Exception ex)
 //                {
-//                    LoggingModule.logException(ex);
+//                    LoggingModule.logException(ex)
 //                }
 //            }
 //        }
 //
 //        private void SetupReferenceMenu()
 //        {
-//            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+//            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService
 //            
 //            if (mcs != null)
 //            {
-//                fsiReferenceMenu = new FsiReferenceCommand(DTE.Value, mcs);
-//                fsiReferenceMenu.SetupCommands();
+//                fsiReferenceMenu = new FsiReferenceCommand(DTE.Value, mcs)
+//                fsiReferenceMenu.SetupCommands()
 //            }
 //        }
 //
 //        private void SetupFolderMenu()
 //        {
-//            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-//            var shell = GetService(typeof(SVsUIShell)) as IVsUIShell;
+//            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService
+//            var shell = GetService(typeof(SVsUIShell)) as IVsUIShell
 //
 //            if (mcs != null)
 //            {
-//                newFolderMenu = new FolderMenuCommands(DTE.Value, mcs, shell);
-//                newFolderMenu.SetupCommands();
+//                newFolderMenu = new FolderMenuCommands(DTE.Value, mcs, shell)
+//                newFolderMenu.SetupCommands()
 //            }
 //        }
 //
 //        private void RegisterPriorityCommandTarget()
 //        {
-//            var rpct = GetService(typeof(SVsRegisterPriorityCommandTarget)) as IVsRegisterPriorityCommandTarget;
-//            rpct.RegisterPriorityCommandTarget(0, newFolderMenu, out pctCookie);
+//            var rpct = GetService(typeof(SVsRegisterPriorityCommandTarget)) as IVsRegisterPriorityCommandTarget
+//            rpct.RegisterPriorityCommandTarget(0, newFolderMenu, out pctCookie)
 //        }
 //
 //        private void UnregisterPriorityCommandTarget()
 //        {
 //            if (pctCookie != 0)
 //            {
-//                var rpct = GetService(typeof(SVsRegisterPriorityCommandTarget)) as IVsRegisterPriorityCommandTarget;
+//                var rpct = GetService(typeof(SVsRegisterPriorityCommandTarget)) as IVsRegisterPriorityCommandTarget
 //                if (rpct != null)
 //                {
-//                    rpct.UnregisterPriorityCommandTarget(pctCookie);
-//                    pctCookie = 0;
+//                    rpct.UnregisterPriorityCommandTarget(pctCookie)
+//                    pctCookie = 0
 //                }
 //            }
 //        }    
@@ -221,9 +221,9 @@ module private useless = ()
 //        {
 //            if (objectManagerCookie == 0)
 //            {
-//                IVsObjectManager2 objManager = GetService(typeof(SVsObjectManager)) as IVsObjectManager2;
-//                if (objManager == null) return;
-//                ErrorHandler.ThrowOnFailure(objManager.RegisterSimpleLibrary(library, out objectManagerCookie));
+//                IVsObjectManager2 objManager = GetService(typeof(SVsObjectManager)) as IVsObjectManager2
+//                if (objManager == null) return
+//                ErrorHandler.ThrowOnFailure(objManager.RegisterSimpleLibrary(library, out objectManagerCookie))
 //            }
 //        }
 //
@@ -231,20 +231,20 @@ module private useless = ()
 //        {
 //            if (objectManagerCookie != 0)
 //            {
-//                IVsObjectManager2 objManager = GetService(typeof(SVsObjectManager)) as IVsObjectManager2;
+//                IVsObjectManager2 objManager = GetService(typeof(SVsObjectManager)) as IVsObjectManager2
 //                if (objManager != null)
-//                    objManager.UnregisterLibrary(objectManagerCookie);
+//                    objManager.UnregisterLibrary(objectManagerCookie)
 //            }
 //        }
 //
 //        public void Dispose()
 //        {
-//            UnregisterPriorityCommandTarget();
-//            UnregisterLibrary();
+//            UnregisterPriorityCommandTarget()
+//            UnregisterLibrary()
 //            if (taskListCommentManager != null)
-//                (taskListCommentManager as IDisposable).Dispose();
+//                (taskListCommentManager as IDisposable).Dispose()
 //            if (fsiReferenceMenu != null)
-//                (fsiReferenceMenu as IDisposable).Dispose();
+//                (fsiReferenceMenu as IDisposable).Dispose()
 //        }
 //    }
 //}
