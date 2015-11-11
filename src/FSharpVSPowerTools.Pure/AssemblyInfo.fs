5025bfc39e597177d1a92729842eeadd7a3b2b38
Jared Hester <jared.hester@gmail.com>
2015-11-11 01:22:07 -0500

    setup project structure

---
+++ src/FSharpVSPowerTools.Pure/AssemblyInfo.fs
@@ -0,0 +1,14 @@
+﻿namespace System
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: InternalsVisibleToAttribute("FSharpVSPowerTools.Tests")>]
+[<assembly: AssemblyTitleAttribute("FSharpVSPowerTools.Pure")>]
+[<assembly: AssemblyProductAttribute("FSharpVSPowerTools")>]
+[<assembly: AssemblyDescriptionAttribute("The Purest Source of F# Power in Visual Studio")>]
+[<assembly: AssemblyVersionAttribute("2.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("2.2.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "2.2.0"
