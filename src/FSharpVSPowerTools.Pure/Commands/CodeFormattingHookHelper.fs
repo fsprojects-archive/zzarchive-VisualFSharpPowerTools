92c337311c702f9c2d5db1f0218b7118305dbc68
Jared Hester <jared.hester@gmail.com>
2015-11-12 00:55:14 -0500

    implemented CodeFormattingHookHelper

--- src/FSharpVSPowerTools.Pure/Commands/CodeFormattingHookHelper.fs
+++ src/FSharpVSPowerTools.Pure/Commands/CodeFormattingHookHelper.fs
@@ -14,39 +14,36 @@ open Microsoft.VisualStudio.Text
 open FSharpVSPowerTools.CodeFormatting      
 open Microsoft.VisualStudio.Shell      
 open FSharpVSPowerTools.ProjectSystem     
-//
-//[<Export (typeof<IWpfTextViewCreationListener>)>]
-//[<Name "F# Formatting Command Hook">]
-//[<ContentType "F#">]
-//[<TextViewRole (PredefinedTextViewRoles.Interactive)>]
-//type CodeFormattingHookHelper [<ImportingConstructor>] 
-//    (   [<Import(typeof<SVsServiceProvider>)>]  
-//        serviceProvider                 :   IServiceProvider,
-//        adaptersFactory                 :   IVsEditorAdaptersFactoryService,  
-//        editorOptionsFactory            :   IEditorOptionsFactoryService,    
-//        editorOperationsFactoryService  :   IEditorOperationsFactoryService, 
-//        textBufferUndoManagerProvider   :   ITextBufferUndoManagerProvider,  
-//        textDocumentFactoryService      :   ITextDocumentFactoryService     ) as self =
-//
-//    member private __.GetServices () =
-//        CodeFormattingServices
-//            (   editorOptionsFactory, editorOperationsFactoryService, textBufferUndoManagerProvider,
-//                textDocumentFactoryService, serviceProvider)
-//
-//    member internal __.RegisterCommandDispatcher (wpfTextView:IWpfTextView) =
-//        maybe{
-//            let! view = adaptersFactory.TryGetViewAdapter wpfTextView
-//            return!
-//                StandardCommandDispatcher.Register (view, wpfTextView, self.GetServices ())
-//        }
-//
-//    interface IWpfTextViewCreationListener with
-//        member __.TextViewCreated (textView:IWpfTextView) : unit = 
-//            System.Windows.Threading.Dispatcher.CurrentDispatcher.BeginInvoke
-//                (Action (fun () -> (self.RegisterCommandDispatcher textView)|>ignore))
-//            |> ignore
-//        
-//        
-//
-//
-//
+
+[<Export (typeof<IWpfTextViewCreationListener>)>]
+[<Name "F# Formatting Command Hook">]
+[<ContentType "F#">]
+[<TextViewRole (PredefinedTextViewRoles.Interactive)>]
+type CodeFormattingHookHelper [<ImportingConstructor>] 
+    (   [<Import(typeof<SVsServiceProvider>)>]  
+        serviceProvider                 :   IServiceProvider,
+        adaptersFactory                 :   IVsEditorAdaptersFactoryService,  
+        editorOptionsFactory            :   IEditorOptionsFactoryService,    
+        editorOperationsFactoryService  :   IEditorOperationsFactoryService, 
+        textBufferUndoManagerProvider   :   ITextBufferUndoManagerProvider,  
+        textDocumentFactoryService      :   ITextDocumentFactoryService     ) as self =
+
+    member private __.GetServices () =
+        CodeFormattingServices
+            (   editorOptionsFactory, editorOperationsFactoryService, textBufferUndoManagerProvider,
+                textDocumentFactoryService, serviceProvider)
+
+    member internal __.RegisterCommandDispatcher (wpfTextView:IWpfTextView) =
+        maybe{
+            let! view = adaptersFactory.TryGetViewAdapter wpfTextView
+            return!
+                StandardCommandDispatcher.Register (view, wpfTextView, self.GetServices ())
+        }
+
+    interface IWpfTextViewCreationListener with
+        member __.TextViewCreated (textView:IWpfTextView) : unit = 
+            System.Windows.Threading.Dispatcher.CurrentDispatcher.BeginInvoke
+                (Action (fun () -> (self.RegisterCommandDispatcher textView)|>ignore))
+            |> ignore
+        
+        
