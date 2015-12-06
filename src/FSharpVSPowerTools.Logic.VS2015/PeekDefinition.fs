369303db5373ca4099c8c1da05e41d89989b9ddd
Vasily Kirichenko <vaskir@gmail.com>
2015-12-06 21:07:01 +0300

    implementing Peek Definition

---
+++ src/FSharpVSPowerTools.Logic.VS2015/PeekDefinition.fs
@@ -0,0 +1,129 @@
+﻿namespace FSharpVSPowerTools.Logic.VS2015
+
+open System
+open System.IO
+open System.ComponentModel.Composition
+open Microsoft.VisualStudio.Language.Intellisense
+open Microsoft.VisualStudio.Text
+open Microsoft.VisualStudio.Utilities
+open Microsoft.VisualStudio.Shell.Interop
+open FSharpVSPowerTools
+open FSharpVSPowerTools.ProjectSystem
+open Microsoft.FSharp.Compiler
+open Microsoft.FSharp.Compiler.SourceCodeServices
+
+type internal DefinitionPeekableItem(span: SnapshotSpan, range: Range.range, peekResultFactory: IPeekResultFactory) =
+    interface IPeekableItem with
+        member __.DisplayName = null
+        member __.Relationships = seq [PredefinedPeekRelationships.Definitions]
+        member __.GetOrCreateResultSource _relationshipName = 
+            { new IPeekResultSource with
+                member __.FindResults(relationshipName, resultCollection, cancellationToken, callback) =
+                    if relationshipName <> PredefinedPeekRelationships.Definitions.Name then ()
+                    else 
+                        let filePath = range.FileName
+                        let fileName = Path.GetFileName filePath
+                        let label = sprintf "%s - (%d, %d)" filePath range.StartLine range.StartColumn
+                        let displayInfo = 
+                            new PeekResultDisplayInfo(label = label, labelTooltip = box filePath, title = fileName, 
+                                                      titleTooltip = filePath)
+                        
+                        let result =
+                            peekResultFactory.Create(
+                                                displayInfo, 
+                                                filePath, 
+                                                span.StartLine.LineNumber,
+                                                span.StartColumn,
+                                                span.EndLine.LineNumber,
+                                                span.EndColumn,
+                                                range.StartLine,
+                                                range.StartColumn, 
+                                                true)
+
+                        resultCollection.Add result
+    //                resultCollection.Add(PeekHelpers.CreateDocumentPeekResult(declarationLocation.Path, declarationLocation.Span, entityOfInterestSpan, _peekableItem.PeekResultFactory));
+    //                callback.ReportProgress(100 * ++processedSourceLocations / sourceLocations.Count);
+            }
+
+type PeekableItemSource
+    (
+        buffer: ITextBuffer,
+        doc: ITextDocument,
+        peekResultFactory: IPeekResultFactory,
+        serviceProvider: System.IServiceProvider,
+        projectFactory: ProjectFactory,
+        vsLanguageService: VSLanguageService
+    ) =
+
+    let getCurrentFilePathProjectAndDoc () =
+        maybe {
+            let filepath = doc.FilePath
+            let dte = serviceProvider.GetService<EnvDTE.DTE, SDTE>()
+            let! doc = dte.GetCurrentDocument(filepath)
+            let! project = projectFactory.CreateForDocument buffer doc
+            return filepath, project, doc
+        }
+
+    let getDefinitionRange (point: SnapshotPoint) =
+        async {
+            let projectItems = 
+                maybe {
+                    let! _, project, doc = getCurrentFilePathProjectAndDoc()
+                    let! span, symbol = vsLanguageService.GetSymbol(point, doc.FullName, project)
+                    return doc.FullName, project, span, symbol 
+                }
+                 
+            match projectItems with
+            | Some (file, project, span, symbol) ->
+                let! symbolUse = vsLanguageService.GetFSharpSymbolUse(span, symbol, file, project, AllowStaleResults.MatchingSource)
+                match symbolUse with
+                | Some (_fsSymbolUse, fileScopedCheckResults) ->
+                    let start = span.Start
+                    let lineStr = start.GetContainingLine().GetText()
+                    let! findDeclResult = fileScopedCheckResults.GetDeclarationLocation(symbol.Line, symbol.RightColumn, lineStr, symbol.Text, preferSignature=false)
+                    return
+                        match findDeclResult with
+                        | FSharpFindDeclResult.DeclFound range -> Some (span, range)
+                        | _ -> None
+                | _ -> return None
+            | _ -> return None
+        }
+
+    interface IPeekableItemSource with
+        member __.AugmentPeekSession (session, peekableItems) =
+            asyncMaybe {
+                do! if String.Equals(session.RelationshipName, PredefinedPeekRelationships.Definitions.Name, 
+                                     StringComparison.OrdinalIgnoreCase) then Some() else None
+                let! triggerPoint = session.GetTriggerPoint(buffer.CurrentSnapshot) |> Option.ofNullable
+                let! symbolSpan, definitionRange = getDefinitionRange triggerPoint
+                peekableItems.Add (DefinitionPeekableItem(symbolSpan, definitionRange, peekResultFactory))
+            } |> Async.RunSynchronously |> ignore
+
+        member __.Dispose() = ()
+
+[<Export(typeof<IPeekableItemSourceProvider>)>]
+[<ContentType "F#">]
+[<Name "F# Peekable Item Provider">]
+[<SupportsPeekRelationship "IsDefinedBy">]
+type PeekableItemSourceProvider
+    [<ImportingConstructor>]
+    (_peekResultFactory: IPeekResultFactory,
+     _textDocumentFactoryService: ITextDocumentFactoryService,
+     _serviceProvider: System.IServiceProvider) =
+//     projectFactory: ProjectFactory,
+//     vsLanguageService: VSLanguageService) =
+
+    do System.Diagnostics.Debug.Fail "PeekableItemSourceProvider ctor"
+
+    interface IPeekableItemSourceProvider with
+        member __.TryCreatePeekableItemSource(_buffer: ITextBuffer) =
+            do System.Diagnostics.Debug.Fail "Getting text doc..."
+            null
+//            match textDocumentFactoryService.TryGetTextDocument buffer with
+//            | true, doc ->
+//                do System.Diagnostics.Debug.Fail "Got doc, creating source provider..."
+//                buffer.Properties.GetOrCreateSingletonProperty(
+//                    fun() -> 
+//                        upcast new PeekableItemSource(buffer, doc, peekResultFactory,
+//                                                      serviceProvider, projectFactory, vsLanguageService))
+//            | _ -> null
