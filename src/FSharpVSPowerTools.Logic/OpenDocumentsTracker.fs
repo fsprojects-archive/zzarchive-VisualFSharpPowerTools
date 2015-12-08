55805292a6278fb4320f5eaa15e9f854009f0096
Vasily Kirichenko <vaskir@gmail.com>
2015-12-08 23:30:24 +0300

    fixing OpenDocumentTracker

--- src/FSharpVSPowerTools.Logic/OpenDocumentsTracker.fs
+++ src/FSharpVSPowerTools.Logic/OpenDocumentsTracker.fs
@@ -13,10 +13,15 @@ type OpenDocument =
     { Document: ITextDocument
       Snapshot: ITextSnapshot 
       Encoding: Encoding
-      LastChangeTime: DateTime }
+      LastChangeTime: DateTime
+      ViewCount: int }
     member x.Text = lazy (x.Snapshot.GetText())
     static member Create document snapshot encoding lastChangeTime = 
-        { Document = document; Snapshot = snapshot; Encoding = encoding; LastChangeTime = lastChangeTime }
+        { Document = document
+          Snapshot = snapshot
+          Encoding = encoding
+          LastChangeTime = lastChangeTime
+          ViewCount = 1 }
 
 type IOpenDocumentsTracker =
     abstract RegisterView: IWpfTextView -> unit
@@ -46,9 +51,13 @@ type OpenDocumentsTracker [<ImportingConstructor>](textDocumentFactoryService: I
                 let path = doc.FilePath
                 
                 let textBufferChanged (args: TextContentChangedEventArgs) =
-                    ForegroundThreadGuard.CheckThread()
-                    openDocuments <- Map.add path (OpenDocument.Create doc args.After doc.Encoding DateTime.UtcNow) openDocuments
-                    documentChanged.Trigger path
+                    match openDocuments |> Map.tryFind path with
+                    | Some doc ->
+                        ForegroundThreadGuard.CheckThread()
+                        openDocuments <- Map.add path { doc with Snapshot = args.After
+                                                                 LastChangeTime = DateTime.UtcNow } openDocuments
+                        documentChanged.Trigger path
+                    | None -> ()
                 
                 let textBufferChangedSubscription = view.TextBuffer.ChangedHighPriority.Subscribe textBufferChanged
                 
@@ -61,8 +70,15 @@ type OpenDocumentsTracker [<ImportingConstructor>](textDocumentFactoryService: I
                 
                 and viewClosedSubscription: IDisposable = view.Closed.Subscribe viewClosed
                 let! lastWriteTime = File.tryGetLastWriteTime path
-                let openDocument = OpenDocument.Create doc view.TextBuffer.CurrentSnapshot doc.Encoding lastWriteTime
-                openDocuments <- Map.add path openDocument openDocuments 
+                
+                let openDocument = 
+                    openDocuments 
+                    |> Map.tryFind path
+                    |> Option.map (fun doc -> { doc with ViewCount = doc.ViewCount + 1 })
+                    |> Option.getOrTry (fun _ ->
+                        OpenDocument.Create doc view.TextBuffer.CurrentSnapshot doc.Encoding lastWriteTime)
+                
+                openDocuments <- Map.add path openDocument openDocuments
             } |> ignore
         
         member __.MapOpenDocuments f = Seq.map f openDocuments
