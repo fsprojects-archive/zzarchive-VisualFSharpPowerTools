88b551ab8bf39be8bf3e1c674a5dea72d9760edb
Vasily Kirichenko <vaskir@gmail.com>
2015-12-09 23:22:44 +0300

    create a distinct HighlightUsageTagger and PrintfSpecifiersUsageTagger for each ITextView, not for each ITextBuffer

--- src/FSharpVSPowerTools/Commands/HighlightUsageTaggerProvider.cs
+++ src/FSharpVSPowerTools/Commands/HighlightUsageTaggerProvider.cs
@@ -48,7 +48,7 @@ namespace FSharpVSPowerTools
             ITextDocument doc;
             if (_textDocumentFactoryService.TryGetTextDocument(buffer, out doc))
             {
-                return buffer.Properties.GetOrCreateSingletonProperty(
+                return textView.Properties.GetOrCreateSingletonProperty(
                     () => new HighlightUsageTagger(doc, textView, _fsharpVsLanguageService, _serviceProvider, _projectFactory)) as ITagger<T>;
             }
 
--- src/FSharpVSPowerTools/Commands/PrintfSpecifiersUsageTaggerProvider.cs
+++ src/FSharpVSPowerTools/Commands/PrintfSpecifiersUsageTaggerProvider.cs
@@ -58,7 +58,7 @@ namespace FSharpVSPowerTools
             ITextDocument doc;
             if (_textDocumentFactoryService.TryGetTextDocument(buffer, out doc))
             {
-                return buffer.Properties.GetOrCreateSingletonProperty(
+                return textView.Properties.GetOrCreateSingletonProperty(
                     () => new PrintfSpecifiersUsageTagger(doc, textView, _fsharpVsLanguageService, _serviceProvider, _projectFactory)) as ITagger<T>;
             }
 
