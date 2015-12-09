591f5281d4020cd919de3a9a9b8d463d00372a46
Vasily Kirichenko <vaskir@gmail.com>
2015-12-09 22:43:38 +0300

    enable SyntaxConstructClassifier and PrintfSpecifiersUsageTagger in Peek Def view

--- src/FSharpVSPowerTools/Commands/PrintfSpecifiersUsageTaggerProvider.cs
+++ src/FSharpVSPowerTools/Commands/PrintfSpecifiersUsageTaggerProvider.cs
@@ -16,8 +16,7 @@ namespace FSharpVSPowerTools
     [Export(typeof(IViewTaggerProvider))]
     [ContentType("F#")]
     [TagType(typeof(PrintfSpecifiersUsageTag))]
-    [TextViewRole(PredefinedTextViewRoles.Editable)]
-    [TextViewRole(PredefinedTextViewRoles.PrimaryDocument)]
+    [TextViewRole(PredefinedTextViewRoles.Interactive)]
     public class PrintfSpecifiersUsageTaggerProvider : IViewTaggerProvider, IDisposable
     {
         readonly IServiceProvider _serviceProvider;
