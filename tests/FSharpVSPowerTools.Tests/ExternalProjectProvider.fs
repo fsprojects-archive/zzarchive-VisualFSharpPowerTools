93b18d17a61042f3a84b6ae11541b47c898b7c33
Vasily Kirichenko <vaskir@gmail.com>
2016-01-16 11:45:15 +0300

    remove reference to FSharp.Compiler.Service.ProjectCrackerTool.exe
    switch from FSharpProjectFileInfo.Parse to ProjectCracker.GetProjectOptionsFromProjectFile
    (wip)

--- tests/FSharpVSPowerTools.Tests/ExternalProjectProvider.fs
+++ tests/FSharpVSPowerTools.Tests/ExternalProjectProvider.fs
@@ -2,29 +2,32 @@
 
 open FSharpVSPowerTools.ProjectSystem
 open FSharpVSPowerTools
-open Microsoft.FSharp.Compiler.SourceCodeServices.ProjectCrackerTool.Program
+open Microsoft.FSharp.Compiler.SourceCodeServices
 
 type ExternalProjectProvider(projectFileName) =    
     // We use Debug mode since test assemblies are created in Debug mode in BuildTests target
-    let projectResolver = FSharpProjectFileInfo.Parse(projectFileName,properties = [("Configuration", "Debug")])
-    let fullProjectFileName = projectResolver.FullPath
-    let frameworkVersion = 
-        projectResolver.FrameworkVersion 
-        |> Option.map (function 
-            | "v2.0" -> FSharpTargetFramework.NET_2_0
-            | "v3.0" -> FSharpTargetFramework.NET_3_0
-            | "v3.5" -> FSharpTargetFramework.NET_3_5
-            | "v4.0" -> FSharpTargetFramework.NET_4_0
-            | "v4.5" -> FSharpTargetFramework.NET_4_5
-            | _ -> FSharpTargetFramework.NET_4_6)
-        |> Option.getOrElse FSharpTargetFramework.NET_4_6
-    let compilerOptions = projectResolver.Options |> List.toArray
-    let sourceFiles = projectResolver.CompileFiles |> List.toArray
-    let outputPath = projectResolver.OutputFile
-    let referencedProjectFileNames = projectResolver.ProjectReferences
+    let projectOptions = ProjectCracker.GetProjectOptionsFromProjectFile(projectFileName, properties = [("Configuration", "Debug")])
+    let fullProjectFileName = projectOptions.ProjectFileName
+    let frameworkVersion = FSharpTargetFramework.NET_4_5
+    let opts = projectOptions.OtherOptions
+    let _ = opts
+//        |> Option.map (function 
+//            | "v2.0" -> FSharpTargetFramework.NET_2_0
+//            | "v3.0" -> FSharpTargetFramework.NET_3_0
+//            | "v3.5" -> FSharpTargetFramework.NET_3_5
+//            | "v4.0" -> FSharpTargetFramework.NET_4_0
+//            | "v4.5" -> FSharpTargetFramework.NET_4_5
+//            | _ -> FSharpTargetFramework.NET_4_6)
+//        |> Option.getOrElse FSharpTargetFramework.NET_4_6
+    let compilerOptions = projectOptions.OtherOptions
+    let sourceFiles = [||] // projectResolver. CompileFiles |> List.toArray
+    //let outputPath = projectResolver.OutputFile
+    let referencedProjects = Array.toList projectOptions.ReferencedProjects
+    let referencedProjectFileNames = referencedProjects |> List.map fst
     let referencedProjects: IProjectProvider list = 
-        referencedProjectFileNames
-        |> List.map (fun projectFile -> ExternalProjectProvider(projectFile) :> _)
+        referencedProjectFileNames 
+        |> List.map (fun projectFile -> ExternalProjectProvider projectFile :> IProjectProvider)
+
     member __.ReferencedProjects = referencedProjects
     interface IProjectProvider with
         member __.IsForStandaloneScript = false
@@ -33,17 +36,7 @@ type ExternalProjectProvider(projectFileName) =
         member __.CompilerOptions = compilerOptions
         member __.CompilerVersion = None
         member __.SourceFiles = sourceFiles
-        member __.FullOutputFilePath = outputPath
+        member __.FullOutputFilePath = Some "" //outputPath
         member __.GetReferencedProjects() = referencedProjects
         member __.GetAllReferencedProjectFileNames() = referencedProjectFileNames
-        member __.GetProjectCheckerOptions languageService =
-            async {
-                let referencedProjectOptions = 
-                    referencedProjects 
-                    |> List.choose (fun p -> 
-                        p.FullOutputFilePath |> Option.map (fun x -> x, p.GetProjectCheckerOptions languageService |> Async.RunSynchronously))
-                    |> List.toArray
-                let opts = languageService.GetProjectCheckerOptions (fullProjectFileName, sourceFiles, compilerOptions, referencedProjectOptions)
-                //printfn "Current project options: %A" opts
-                return opts
-            }
+        member __.GetProjectCheckerOptions _ = async.Return projectOptions
