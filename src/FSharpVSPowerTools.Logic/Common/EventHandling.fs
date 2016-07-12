c939047e1992ba46e08bad11f7f730143ebb90cb
Vasily Kirichenko <vaskir@gmail.com>
2016-07-12 22:12:35 +0300

    use AsyncManualResetEvent instead of Async.AwaitEvent to solve missing events problem

--- src/FSharpVSPowerTools.Logic/Common/EventHandling.fs
+++ src/FSharpVSPowerTools.Logic/Common/EventHandling.fs
@@ -6,6 +6,8 @@ open Microsoft.VisualStudio.Text.Editor
 open System.Threading
 open System.Windows.Threading
 open FSharpVSPowerTools
+open System.Diagnostics
+open System.Threading.Tasks
 
 [<RequireQualifiedAccess>]
 module ViewChange =    
@@ -32,10 +34,36 @@ type CallInUIContext =
                 do! Async.SwitchToContext ctx
             })
 
+type AsyncManualResetEvent() = 
+    [<VolatileField>]
+    let mutable _tcs = new TaskCompletionSource<bool>()
+    
+    member __.WaitAsync() = _tcs.Task |> Async.AwaitTask |> Async.Ignore
+    
+    member __.Reset() =
+        let rec loop() =
+            let tcs = _tcs
+            if tcs.Task.IsCompleted &&
+               Interlocked.CompareExchange(&_tcs, new TaskCompletionSource<bool>(), tcs) <> tcs then 
+                loop()
+        loop()
+
+    member __.Set() =
+        let tcs = _tcs
+        Task.Factory.StartNew(
+            (fun s -> (box s :?> TaskCompletionSource<bool>).TrySetResult true),
+            tcs, 
+            CancellationToken.None, 
+            TaskCreationOptions.PreferFairness, 
+            TaskScheduler.Default) |> ignore
+        tcs.Task.Wait()
+
 type DocumentEventListener (events: IEvent<unit> list, delayMillis: uint16, update: CallInUIContext -> Async<unit>) =
     // Start an async loop on the UI thread that will execute the update action after the delay
     do if List.isEmpty events then invalidArg "events" "Events must be a non-empty list"
     let events = events |> List.reduce Event.merge
+    let triggered = AsyncManualResetEvent()
+    do events.Add (fun _ -> triggered.Set())
     let timer = DispatcherTimer(DispatcherPriority.ApplicationIdle, Interval = TimeSpan.FromMilliseconds (float delayMillis))
     let tokenSource = new CancellationTokenSource()
     let mutable disposed = false
@@ -63,18 +91,22 @@ type DocumentEventListener (events: IEvent<unit> list, delayMillis: uint16, upda
 
        let computation =
            async { 
-               let cts = ref (new CancellationTokenSource())
-               startUpdate !cts
+               let mutable cts = new CancellationTokenSource()
+               startUpdate cts
 
                while true do
-                   do! Async.AwaitEvent events
+                   //let e = events
+                   do! triggered.WaitAsync()
+                   triggered.Reset()
+                   //Logging.logInfo <| fun _ -> "!!!!!!!!! awaited"
                    if not skipTimerDelay then
                        startNewTimer()
                        do! awaitPauseAfterChange()
-                   (!cts).Cancel()
-                   (!cts).Dispose()
-                   cts := new CancellationTokenSource()
-                   startUpdate !cts }
+                   cts.Cancel()
+                   cts.Dispose()
+                   cts <- new CancellationTokenSource()
+                   startUpdate cts 
+           }
        
        Async.StartInThreadPoolSafe (computation, tokenSource.Token)
 
