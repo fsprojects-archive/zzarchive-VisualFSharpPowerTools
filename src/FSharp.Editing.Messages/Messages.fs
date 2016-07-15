baff136337dcb6053393e83a505e0db846500942
Vasily Kirichenko <vaskir@gmail.com>
2016-07-15 21:16:13 +0300

    cosmetics

--- src/FSharp.Editing.Messages/Messages.fs
+++ src/FSharp.Editing.Messages/Messages.fs
@@ -16,31 +16,34 @@ type ServerCapability =
     | HighlighIdentifier
     | ImplementInterface
 
-type ClientNotification =
-      /// New solution loaded or new folder opened. All server state should be discarded.
-    | Cleanup
-      /// A new project loaded or a known project changed in any way (name, file list, references, etc.)
-    | ProjectLoaded of Project
-      /// Previously loaded project was unloaded from IDE or deleted from disk so it should not be part of solution anymore.
-    | ProjectUnloaded
-      /// Document opened in an IDE for the first time. No need to send its content since server can read it from disk.
-    | DocumentOpened of file: FilePath
-      /// Document closed. Server can stop tracking its content in a special way and just read it from disk.
-    | DocumentClosed of file: FilePath
-    | DocumentRenamed of oldFile: FilePath * newFile: FilePath
-      /// Opened document changed in IDE without saving to disk. Full new content should be sent 
-      /// (for now. Later we can add incremental changes support).
-    | DocumentChanged of file: FilePath * content: DocumentContent
-    | Subscribe of ServerCapability Set
-
 type SyntaxHighlightingInfo = 
     { ClassificationRanges: unit list }
 
 type ServerNotification =
     | SyntaxHighlightingChanged of file: FilePath * SyntaxHighlightingInfo
 
-type RequestMessage = 
+type ClientCallbackAddress = string
+
+type ClientNotification =
+      /// Reset all server state.
+    | Reset
+      /// A new project loaded or a known project changed in any way (name, file list, references, etc.)
+    | ProjectLoaded of Project
+      /// Previously loaded project was unloaded from IDE or deleted from disk so it should not be part of solution anymore.
+    | ProjectUnloaded of FilePath
+      /// Document opened in an IDE for the first time. No need to send its content since server can read it from disk.
+    | DocumentOpened of FilePath
+      /// Document closed. Server can stop tracking its content in a special way and just read it from disk.
+    | DocumentClosed of FilePath
+    | DocumentRenamed of oldFile: FilePath * newFile: FilePath
+      /// Opened document changed in IDE without saving to disk. Full new content should be sent 
+      /// (for now. Later we can add incremental changes support).
+    | DocumentChanged of FilePath * DocumentContent
+      /// Client subscribing on a set of server notifications, passing an address to which them should be sent.
+    | Subscribe of Set<ServerCapability> * ClientCallbackAddress
+
+type ClientRequest = 
     { RequestId: RequestId }
 
-type ResponseMessage = 
+type ServerResponse = 
     { RequestId: RequestId }
