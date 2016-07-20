11bf10a7b36741c58f1330dbb42d527d3c296144
Jared Hester <jared.hester@gmail.com>
2016-07-19 23:16:23 -0400

    migrate implementations to FSharp.Editing
    - the new navigation cache will be built around projects instead of
    caches for a solution
    - renamed to buffertracker to stop collisions and allow implementation
    change
    -

--- src/FSharp.Editing.VisualStudio/Navigation/NavigableItemCache.fs
+++ src/FSharp.Editing.VisualStudio/Navigation/NavigableItemCache.fs
@@ -5,9 +5,7 @@ open System.Collections.Concurrent
 open EnvDTE80
 open Microsoft.VisualStudio.Shell.Interop
 open EnvDTE
-open FSharp.Editing
 open FSharp.Editing.Navigation
-open FSharp.Editing.ProjectSystem
 open FSharp.Editing.VisualStudio
 open System.Security.Cryptography
 open System.Text
@@ -16,15 +14,6 @@ open System.IO
 open System.Threading
 open FSharp.Editing
 
-type Source = string
-
-type FileDescriptor =
-    { Path: FilePath
-      LastWriteTime: DateTime }
-
-type FileNavigableItems =
-    { Descriptor: FileDescriptor
-      Items: NavigableItem[] }
 
 type NavigableItemCache (serviceProvider: System.IServiceProvider) =
     let cache = ConcurrentDictionary<FilePath, FileNavigableItems>(StringComparer.Ordinal)
--- src/FSharp.Editing/ProjectSystem/FileSystem.fs
+++ src/FSharp.Editing/ProjectSystem/FileSystem.fs
@@ -8,56 +8,56 @@ open FSharp.Editing
 type Version = int
 
 
-type FileSystem (editorBufferTracker: IBufferTracker) =
+type FileSystem (openDocumentsTracker: IBufferTracker<_>) =
     static let defaultFileSystem = Shim.FileSystem
 
     let getOpenDocContent (fileName: string) =
-        editorBufferTracker.TryFindEditorBuffer fileName
-        |> Option.map (fun doc -> 
+        openDocumentsTracker.TryFindEditorBuffer fileName
+        |> Option.map (fun doc ->
             doc.Text |> doc.Encoding.GetBytes)
 
     interface IFileSystem with
-        member __.FileStreamReadShim fileName = 
+        member __.FileStreamReadShim fileName =
             getOpenDocContent fileName
             |> Option.map (fun bytes -> new MemoryStream(bytes) :> Stream)
             |> Option.getOrTry (fun () -> defaultFileSystem.FileStreamReadShim fileName)
-        
+
         member __.ReadAllBytesShim fileName =
-            getOpenDocContent fileName 
+            getOpenDocContent fileName
             |> Option.getOrTry (fun () -> defaultFileSystem.ReadAllBytesShim fileName)
-        
+
         member __.GetLastWriteTimeShim fileName =
-            editorBufferTracker.TryFindEditorBuffer fileName
+            openDocumentsTracker.TryFindEditorBuffer fileName
             |> Option.bind (fun doc ->
                 if doc.IsDirty then Some doc.LastChangeTime else None)
             |> Option.getOrTry (fun () -> defaultFileSystem.GetLastWriteTimeShim fileName)
-        
-        member __.GetTempPathShim () = 
+
+        member __.GetTempPathShim () =
             defaultFileSystem.GetTempPathShim ()
-        
-        member __.FileStreamCreateShim fileName = 
+
+        member __.FileStreamCreateShim fileName =
             defaultFileSystem.FileStreamCreateShim fileName
-        
-        member __.FileStreamWriteExistingShim fileName = 
+
+        member __.FileStreamWriteExistingShim fileName =
             defaultFileSystem.FileStreamWriteExistingShim fileName
-        
-        member __.GetFullPathShim fileName = 
+
+        member __.GetFullPathShim fileName =
             defaultFileSystem.GetFullPathShim fileName
-        
-        member __.IsInvalidPathShim fileName = 
+
+        member __.IsInvalidPathShim fileName =
             defaultFileSystem.IsInvalidPathShim fileName
-        
-        member __.IsPathRootedShim fileName = 
+
+        member __.IsPathRootedShim fileName =
             defaultFileSystem.IsPathRootedShim fileName
 
-        member __.SafeExists fileName = 
+        member __.SafeExists fileName =
             defaultFileSystem.SafeExists fileName
-        
-        member __.FileDelete fileName = 
+
+        member __.FileDelete fileName =
             defaultFileSystem.FileDelete fileName
-        
-        member __.AssemblyLoadFrom fileName = 
+
+        member __.AssemblyLoadFrom fileName =
             defaultFileSystem.AssemblyLoadFrom fileName
 
-        member __.AssemblyLoad assemblyName = 
+        member __.AssemblyLoad assemblyName =
             defaultFileSystem.AssemblyLoad assemblyName
