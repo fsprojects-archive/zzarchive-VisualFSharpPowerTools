e81e25aca6a5929659e2741f3ceebfb3aadc3d71
Jared Hester <jared.hester@gmail.com>
2016-07-30 23:21:55 -0400

    parallel rename tests

--- tests/FSharp.Editing.Tests/RenameTests.fs
+++ tests/FSharp.Editing.Tests/RenameTests.fs
@@ -7,37 +7,37 @@ open FSharp.Editing
 let shouldStaysAsIs symbolKind name = encapsulateIdentifier symbolKind name |> assertEqual name
 let shouldBeEncapsulated symbolKind name = encapsulateIdentifier symbolKind name |> assertEqual ("``" + name + "``")
 
-[<Test>]
+[<Test; Parallelizable>]
 let ``should not encapsulate normal identifiers``() = 
     shouldStaysAsIs SymbolKind.Ident "abc"
     shouldStaysAsIs SymbolKind.Ident "abc1234"
     shouldStaysAsIs SymbolKind.Ident "a_4"
 
-[<Test>]
+[<Test; Parallelizable>]
 let ``should encapsulate keywords``() = 
     shouldBeEncapsulated SymbolKind.Ident "namespace"
     shouldBeEncapsulated SymbolKind.Ident "module"
     shouldBeEncapsulated SymbolKind.Ident "let"
 
-[<Test>]
+[<Test; Parallelizable>]
 let ``should encapsulate special chars``() = 
     shouldBeEncapsulated SymbolKind.Ident "this is a valid identifierer"
     shouldBeEncapsulated SymbolKind.Ident "look!" // reserved for future F#
 
-[<Test>]
+[<Test; Parallelizable>]
 let ``should not encapsulate already encapsulated identifiers``() = 
     shouldStaysAsIs SymbolKind.Ident "``this is already encapsulated``"
     shouldStaysAsIs SymbolKind.Ident "``this``"
 
-[<Test>]
+[<Test; Parallelizable>]
 let ``should not encapsulate operators``() = 
     shouldStaysAsIs SymbolKind.Operator "</>"
     shouldStaysAsIs SymbolKind.Operator "*"
 
-[<Test>]
+[<Test; Parallelizable>]
 let ``should not encapsulate generic type parameters``() = 
     shouldStaysAsIs SymbolKind.GenericTypeParameter "'a"
 
-[<Test>]
+[<Test; Parallelizable>]
 let ``should not encapsulate statically resolved type parameters``() = 
     shouldStaysAsIs SymbolKind.StaticallyResolvedTypeParameter "'a"
