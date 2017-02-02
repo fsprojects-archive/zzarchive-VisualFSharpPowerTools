2c1d6be242fa72354efd0f24805e7beb5486e6d7
ReedCopsey <reed@reedcopsey.com>
2017-02-02 15:39:23 -0800

    Update Version to 2.5.6

--- src/FSharpVSPowerTools/Properties/AssemblyInfo.cs
+++ src/FSharpVSPowerTools/Properties/AssemblyInfo.cs
@@ -6,15 +6,15 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitleAttribute("FSharpVSPowerTools")]
 [assembly: AssemblyProductAttribute("FSharpVSPowerTools")]
 [assembly: AssemblyDescriptionAttribute("A collection of additional commands for F# in Visual Studio")]
-[assembly: AssemblyVersionAttribute("2.5.5")]
-[assembly: AssemblyFileVersionAttribute("2.5.5")]
+[assembly: AssemblyVersionAttribute("2.5.6")]
+[assembly: AssemblyFileVersionAttribute("2.5.6")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String InternalsVisibleTo = "FSharp.Editing.VisualStudio.Tests";
         internal const System.String AssemblyTitle = "FSharpVSPowerTools";
         internal const System.String AssemblyProduct = "FSharpVSPowerTools";
         internal const System.String AssemblyDescription = "A collection of additional commands for F# in Visual Studio";
-        internal const System.String AssemblyVersion = "2.5.5";
-        internal const System.String AssemblyFileVersion = "2.5.5";
+        internal const System.String AssemblyVersion = "2.5.6";
+        internal const System.String AssemblyFileVersion = "2.5.6";
     }
 }
